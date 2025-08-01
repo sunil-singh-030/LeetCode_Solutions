f9358febcd2d2f22dbf067269f981922589a28de
Sunil Singh <sunil.singh.030250@gmail.com>
2025-08-01 07:13:02 +0530

    Create 18-4sum

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,54 @@
+// Last updated: 8/1/2025, 7:12:54 AM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) return result;
+
+        Arrays.sort(nums); // Sorting the array
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            // Avoid duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // Early termination
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if ((long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                // Avoid duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                // Early termination
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if ((long) nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // Skip duplicates for the third and fourth numbers
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
