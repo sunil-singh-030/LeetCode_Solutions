fb2b85d34938f164dadf82d5f66ceda62fd24eb7
Sunil Singh <sunil.singh.030250@gmail.com>
2025-08-01 07:09:45 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.java
@@ -0,0 +1,22 @@
+// Last updated: 8/1/2025, 7:09:38 AM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int left=0;
+        int right=matrix[0].length-1;
+        while (left<matrix.length && right>=0){
+            int num=matrix[left][right];
+            if (num==target){
+                return true;
+            }
+            else{
+                if (num>target){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return false;
+    }
+}
