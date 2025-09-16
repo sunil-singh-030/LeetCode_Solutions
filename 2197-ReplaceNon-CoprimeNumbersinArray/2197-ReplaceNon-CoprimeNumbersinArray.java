dddc68fff67dfab14f2610beae05ac380686730d
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-16 10:33:53 +0530

    Create 2197-ReplaceNon-CoprimeNumbersinArray

---
+++ 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.java
@@ -0,0 +1,26 @@
+// Last updated: 9/16/2025, 10:33:49 AM
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums) {
+            // Continuously merge with the last element of the list if they are not coprime
+            while (!result.isEmpty() && gcd(result.get(result.size() - 1), num) > 1) {
+                num = (int) lcm(result.remove(result.size() - 1), num);
+            }
+            result.add(num);
+        }
+        return result;
+    }
+    public static long lcm(int a,int b){
+		
+		return (long) ( (long) a*b)/gcd(a,b);
+    }
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
