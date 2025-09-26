669de94a2f6011c2a23719308e3c9f0add19bb67
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-26 13:49:27 +0530

    File updated at 9/26/2025, 1:49:28 PM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,39 +1,37 @@
-// Last updated: 9/26/2025, 1:31:23 PM
+// Last updated: 9/26/2025, 1:49:28 PM
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ls=new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> ls = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i=0;i<nums.length-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for (int i=0 ; i<n-2 ; i++){
+            if (i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            if (nums[i]>0){
-                break;
-            }
-            int left=i+1;
-            int right=nums.length-1;
-            while (left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if (sum==0){
-                    ls.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    // Skip duplicates for the second number
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    
-                    // Skip duplicates for the third number
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    left++;
-                    right--;
-                    
+            int j = i+1;
+            int k = n-1;
+            while (j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if (sum>0){
+                    k--;
                 }
                 else if (sum<0){
-                    left++;
+                    j++;
                 }
                 else{
-                    right--;
+                    List<Integer> currls = new ArrayList<>();
+                    currls.add(nums[i]);
+                    currls.add(nums[j]);
+                    currls.add(nums[k]);
+                    ls.add(currls);
+                    while (j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while (j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
                 }
             }
         }
