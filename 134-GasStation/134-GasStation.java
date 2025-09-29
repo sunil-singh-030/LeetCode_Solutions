541d605f49350fb6d49ea9164767ef0483c8e618
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-29 14:16:54 +0530

    File updated at 9/29/2025, 2:16:52 PM

--- 134-GasStation/134-GasStation.java
+++ 134-GasStation/134-GasStation.java
@@ -1,23 +1,31 @@
-// Last updated: 9/29/2025, 2:08:04 PM
+// Last updated: 9/29/2025, 2:16:52 PM
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total=0;
-        for (int i=0;i<cost.length;i++){
-            total+=gas[i]-cost[i];
+        int n = cost.length;
+        int total = 0;
+        for (int i=0 ; i<n ; i++){
+            total += gas[i]-cost[i];
         }
-        if(total<0){
-            return -1;
+        if (total<0){
+            return -1 ;
         }
-        int idx=0;
-        int curr=0;
-        for (int i=0;i<cost.length;i++){
-            curr+=gas[i]-cost[i];
+        int curr = 0;
+        int idx = 0;
+        int need = 0;
+        for (int i=0 ; i<n ; i++){
+            curr += (gas[i]-cost[i]);
             if (curr<0){
-                curr=0;
-                idx=i+1;
+                idx = i+1;
+                need += curr;
+                curr = 0;
             }
         }
+        if (need+curr<0){
+            return -1;
+        }
         return idx;
+
+        
 
     }
 }
