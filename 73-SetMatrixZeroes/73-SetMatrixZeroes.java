df4bda833f06b5b797611df1768506f1902a32aa
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-29 11:15:58 +0530

    File updated at 9/29/2025, 11:15:57 AM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,23 +1,56 @@
-// Last updated: 8/1/2025, 7:11:52 AM
+// Last updated: 9/29/2025, 11:15:57 AM
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int [] row1=new int[matrix.length];
-        int [] col1=new int[matrix[0].length];
-        for (int row=0;row<matrix.length;row++){
-            for (int col=0;col<matrix[0].length;col++){
-                if (matrix[row][col]==0){
-                    row1[row]=1;
-                    col1[col]=1;
+        boolean first_r_0 = false;
+        boolean first_c_0 = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i=0 ; i<m ; i++){
+            if (matrix[i][0]==0){
+                first_c_0 = true;
+                break;
+            }
+        }
+        for (int i=0 ; i<n ; i++){
+            if (matrix[0][i]==0){
+                first_r_0 = true;
+                break;
+            }
+        }
+        for (int i=0 ; i<m ; i++){
+            for (int j=0 ; j<n ; j++){
+                if (matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int row=0;row<matrix.length;row++){
-            for (int col=0;col<matrix[0].length;col++){
-                if (row1[row]==1 || col1[col]==1){
-                    matrix[row][col]=0;
+        // for (int[] a : matrix){
+        //     System.out.println(Arrays.toString(a));
+        // }
+        for (int i=1 ; i<m ; i++){
+            if (matrix[i][0]==0){
+                for (int j=0 ; j<n ; j++){
+                    matrix[i][j] = 0;
                 }
             }
         }
-        
+        for (int i=1 ; i<n ; i++){
+            if (matrix[0][i]==0){
+                for (int j=0 ; j<m ; j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        if (first_r_0){
+            for (int i=0 ; i<n ; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if (first_c_0){
+            for (int i=0 ; i<m ; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
