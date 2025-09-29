081326820b299c644c30e1ccc27dabeeff392e3b
Sunil Singh <sunil.singh.030250@gmail.com>
2025-09-29 13:50:28 +0530

    File updated at 9/29/2025, 1:50:27 PM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,44 +1,25 @@
-// Last updated: 9/29/2025, 1:48:24 PM
+// Last updated: 9/29/2025, 1:50:27 PM
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        for (int i=0 ; i<m ; i++){
-            for (int j=0 ; j<n ; j++){
-                int[][] isvalid = new int[m][n];
-                boolean[] found = new boolean[1];
-                if (isExist(board,i,j,word,0,isvalid,found)){
-                    return true;
-                }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backtrack(board, word, i, j, 0)) return true;
             }
         }
         return false;
     }
-    public boolean isExist(char[][] board, int i, int j, String word, int idx, int[][] isvalid, boolean[] found ){
-        if (found[0]){
-            return true;
-        }
-        if (idx==word.length()){
-            return true;
-        }
-        if (i<0 || i>=board.length || j<0 || j>=board[0].length || isvalid[i][j]==1){
+
+    boolean backtrack(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) return true;
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index))
             return false;
-        }
-        
-        if (board[i][j]==word.charAt(idx)){
-            isvalid[i][j] = 1;
-            int[] r = {0,1,0,-1};
-            int[] c = {1,0,-1,0};
-            for (int k=0 ; k<4 ; k++){
-                boolean currans = isExist(board,i+r[k],j+c[k],word,idx+1,isvalid,found);
-                if (currans){
-                    found[0] = true;
-                    return true;
-                }
-            }
-            isvalid[i][j] = 0;
-        }
-        return false;
+        char temp = board[i][j];
+        board[i][j] = '#';
+        boolean found = backtrack(board, word, i + 1, j, index + 1) ||
+                        backtrack(board, word, i - 1, j, index + 1) ||
+                        backtrack(board, word, i, j + 1, index + 1) ||
+                        backtrack(board, word, i, j - 1, index + 1);
+        board[i][j] = temp;
+        return found;
     }
-    
 }
