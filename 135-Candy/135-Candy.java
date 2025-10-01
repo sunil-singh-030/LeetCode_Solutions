31a414d933b4bfab0b9a43ed47f3d892965780af
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-01 10:51:35 +0530

    File updated at 10/1/2025, 10:51:34 AM

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,4 +1,4 @@
-// Last updated: 10/1/2025, 10:45:12 AM
+// Last updated: 10/1/2025, 10:51:34 AM
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
@@ -14,7 +14,6 @@ class Solution {
                 }
             }
         }
-        //System.out.println(Arrays.toString(ans));
         for (int i=n-2 ; i>=0 ; i--){
             if (ratings[i]>ratings[i+1]){
                 if (ans[i]>ans[i+1]){
@@ -25,7 +24,6 @@ class Solution {
                 }
             }
         }
-        //System.out.println(Arrays.toString(ans));
         int cnt = 0;
         for (int candy : ans){
             cnt += candy;
