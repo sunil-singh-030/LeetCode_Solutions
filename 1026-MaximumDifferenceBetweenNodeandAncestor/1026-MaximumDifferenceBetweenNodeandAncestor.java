4ac12ed05f341ba568af57d37bb43de329bf7868
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-01 13:38:44 +0530

    File updated at 10/1/2025, 1:38:44 PM

--- 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.java
+++ 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.java
@@ -1,4 +1,4 @@
-// Last updated: 10/1/2025, 1:37:28 PM
+// Last updated: 10/1/2025, 1:38:44 PM
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,17 +16,22 @@
  */
 class Solution {
     public int maxAncestorDiff(TreeNode root) {
-        int[] ans = new int[1];
-        fn(root,root.val,root.val,ans);
-        return ans[0];
-    }
-    public void fn(TreeNode root, int lb, int rb, int[] ans){
-        if (root==null){
-            return;
+        if (root == null) {
+            return 0;
         }
-        int currMaxDiff = Math.max(Math.abs(root.val-lb),Math.abs(root.val-rb));
-        ans[0] = Math.max(ans[0],currMaxDiff);
-        fn(root.left,Math.min(lb,root.val),Math.max(rb,root.val),ans);
-        fn(root.right,Math.min(lb,root.val),Math.max(rb,root.val),ans);
+        
+        return helper(root, root.val, root.val); 
+    }
+    
+    private int helper(TreeNode node, int curMax, int curMin) {
+        if (node == null) {
+            return curMax - curMin;
+        }
+        
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+        int left = helper(node.left, curMax, curMin);
+        int right = helper(node.right, curMax, curMin);
+        return Math.max(left, right);
     }
 }
