4645af51ac0a5c6ace2be7204083358d46369400
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-02 08:07:41 +0530

    File updated at 10/2/2025, 8:07:41 AM

--- 1518-WaterBottles/1518-WaterBottles.java
+++ 1518-WaterBottles/1518-WaterBottles.java
@@ -1,12 +1,11 @@
-// Last updated: 10/1/2025, 10:15:36 AM
+// Last updated: 10/2/2025, 8:07:41 AM
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int cnt = 0;
         while (numBottles>=numExchange){
-            cnt += ((numBottles/numExchange)*numExchange);
-            int left = numBottles - ((numBottles/numExchange)*numExchange);
-            left += (numBottles/numExchange);
-            numBottles = left;
+            cnt += numExchange;
+            numBottles -= numExchange;
+            numBottles++;
         }
         cnt += numBottles;
         return cnt;
