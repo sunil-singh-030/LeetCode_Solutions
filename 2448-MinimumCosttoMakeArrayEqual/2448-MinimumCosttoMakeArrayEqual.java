aa5f109a4d2a9b5138e3940b8d7a2959633362b1
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-14 10:39:31 +0530

    File updated at 10/14/2025, 10:39:30 AM

--- 2448-MinimumCosttoMakeArrayEqual/2448-MinimumCosttoMakeArrayEqual.java
+++ 2448-MinimumCosttoMakeArrayEqual/2448-MinimumCosttoMakeArrayEqual.java
@@ -1,44 +1,34 @@
-// Last updated: 10/14/2025, 10:37:17 AM
+// Last updated: 10/14/2025, 10:39:30 AM
 class Solution {
     public long minCost(int[] nums, int[] cost) {
+        long minCost = Long.MAX_VALUE;
+        int min = 1;
+        int max = Integer.MAX_VALUE;
         int n = nums.length;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        
-        // find min and max
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
+        for (int num : nums){
+            min = Math.min(min,num);
+            max = Math.max(max,num);
         }
-
-        long ans = Long.MAX_VALUE;
-        
-        // binary search on possible target value
-        while (min < max) {
-            int mid = min + (max - min) / 2;
-            long cost1 = totalCost(nums, cost, mid);
-            long cost2 = totalCost(nums, cost, mid + 1);
-
-            ans = Math.min(cost1, cost2);
-            
-            // move toward smaller cost direction
-            if (cost1 < cost2) {
+        while (min<max){
+            int mid = (min+max)/2;
+            long cost1 = fn(nums,cost,mid);
+            long cost2 = fn(nums,cost,mid+1);
+            minCost = Math.min(cost1,cost2);
+            if (cost1<cost2){
                 max = mid;
-            } else {
+            }
+            else{
                 min = mid + 1;
             }
         }
-
-        // compute final answer for convergence point
-        ans = Math.min(ans, totalCost(nums, cost, min));
-        return ans;
+        return minCost;
     }
-
-    private long totalCost(int[] nums, int[] cost, long target) {
-        long total = 0;
-        for (int i = 0; i < nums.length; i++) {
-            total += Math.abs(nums[i] - target) * (long) cost[i];
+    public long fn(int[] nums, int[] cost , int x){
+        long totalCost = 0;
+        int n = nums.length;
+        for (int i=0 ; i<n; i++){
+            totalCost += (((long) Math.abs(nums[i]-x))*((long) cost[i]));
         }
-        return total;
+        return totalCost;
     }
 }
