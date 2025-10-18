3e20916ba1c087fe9127d7edd28de020f7662ada
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-18 22:39:41 +0530

    File updated at 10/18/2025, 10:39:40 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,53 +1,54 @@
-// Last updated: 8/1/2025, 7:13:10 AM
+// Last updated: 10/18/2025, 10:39:40 PM
 class Solution {
     public String longestPalindrome(String s) {
-        String out="";
         int n = s.length();
+        String maxPalinStr = "";
+        int maxLen = 0;
+        // when length is odd
         for (int i=0 ; i<n ; i++){
-            String str = ""+s.charAt(i);
-            int left = i-1;
-            int right = i+1;
-            while (left>=0 && right<n){
-                char ch1 = s.charAt(left);
-                char ch2 = s.charAt(right);
-                if (ch1==ch2){
-                    str = (ch1+str+ch2);
+            int leftPointer = i-1;
+            int rightPointer = i+1;
+            int currlen = 1; // 5 2 1 2
+            while (leftPointer>=0 && rightPointer<n){
+                if (s.charAt(leftPointer)==s.charAt(rightPointer)){
+                    currlen += 2;
                 }
                 else{
                     break;
                 }
-                left--;
-                right++;
+                leftPointer--;
+                rightPointer++;
             }
-            if (str.length()>out.length()){
-                out = str;
+            if (currlen>maxLen){
+                maxPalinStr = s.substring(leftPointer+1,leftPointer+1+currlen);
+                maxLen = currlen;
             }
         }
-        if (s.length()>1){
-            for (int i=1 ; i<n ; i++){
-                if (s.charAt(i)==s.charAt(i-1)){
-                    String str = s.substring(i-1,i+1);
-                    int left = i-2;
-                    int right = i+1;
-                    while (left>=0 && right<n){
-                        char ch1 = s.charAt(left);
-                        char ch2 = s.charAt(right);
-                        if (ch1==ch2){
-                            str = (ch1+str+ch2);
-                        }
-                        else{
-                            break;
-                        }
-                        left--;
-                        right++;
-                    }
-                    if (str.length()>out.length()){
-                        out = str;
-                    }
+
+        // 2nd case len is even
+        for (int i=1 ; i<n ; i++){
+            int currLen = 0;
+            int leftPointer = i-1;
+            int rightPointer = i;
+            while (leftPointer>=0 && rightPointer<n){
+                if (s.charAt(leftPointer)==s.charAt(rightPointer)){
+                    currLen += 2;
                 }
+                else{
+                    break;
+                }
+                leftPointer--;
+                rightPointer++;
+            } // 4 5 (4,7)
+            if (currLen>maxLen){
+                maxPalinStr = s.substring(leftPointer+1,leftPointer+1+currLen);
+                maxLen = currLen;
             }
         }
-        return out;
+        return maxPalinStr;
     }
+
+    
+    
     
 }
