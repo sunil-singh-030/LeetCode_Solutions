f958601b612be91758ba4247fd685fdffbbc441c
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-18 12:25:39 +0530

    File updated at 10/18/2025, 12:25:36 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,53 +1,32 @@
-// Last updated: 9/29/2025, 11:16:03 AM
+// Last updated: 10/18/2025, 12:25:36 PM
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean first_r_0 = false;
-        boolean first_c_0 = false;
         int m = matrix.length;
         int n = matrix[0].length;
-        for (int i=0 ; i<m ; i++){
-            if (matrix[i][0]==0){
-                first_c_0 = true;
-                break;
-            }
-        }
-        for (int i=0 ; i<n ; i++){
-            if (matrix[0][i]==0){
-                first_r_0 = true;
-                break;
-            }
-        }
+        boolean[] rowHasZero = new boolean[m];
+        boolean[] colHasZero = new boolean[n];
         for (int i=0 ; i<m ; i++){
             for (int j=0 ; j<n ; j++){
                 if (matrix[i][j]==0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    rowHasZero[i] = true;
+                    colHasZero[j] = true;
                 }
             }
         }
-        for (int i=1 ; i<m ; i++){
-            if (matrix[i][0]==0){
+        for (int i=0 ; i<m ; i++){
+            if (rowHasZero[i]){
                 for (int j=0 ; j<n ; j++){
                     matrix[i][j] = 0;
                 }
             }
         }
-        for (int i=1 ; i<n ; i++){
-            if (matrix[0][i]==0){
+        for (int i=0 ; i<n ; i++){
+            if (colHasZero[i]){
                 for (int j=0 ; j<m ; j++){
                     matrix[j][i] = 0;
                 }
             }
         }
-        if (first_r_0){
-            for (int i=0 ; i<n ; i++){
-                matrix[0][i] = 0;
-            }
-        }
-        if (first_c_0){
-            for (int i=0 ; i<m ; i++){
-                matrix[i][0] = 0;
-            }
-        }
+        
     }
 }
