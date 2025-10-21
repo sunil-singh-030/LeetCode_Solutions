b20465ace31a188f585b3128ee7177ad6a732b27
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-21 13:07:46 +0530

    File updated at 10/21/2025, 1:07:44 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,35 +1,43 @@
-// Last updated: 8/1/2025, 7:12:06 AM
+// Last updated: 10/21/2025, 1:07:44 PM
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b) -> {
+        int n = intervals.length;
+        Arrays.sort(intervals,(a,b)->{
             if (a[0]!=b[0]){
-                return Integer.compare(a[0],b[0]);
+                return a[0]-b[0];
             }
             else{
-                return Integer.compare(a[1],b[1]);
+                return a[1]-b[1];
             }
-        }
-        );
-        List<int[]> ls = new ArrayList<>();
-        for (int i=1 ; i<intervals.length ; i++){
-            int s2 = intervals[i][0];
-            int s1 = intervals[i-1][0];
-            int e2 = intervals[i][1];
-            int e1 = intervals[i-1][1];
-            if (e1>=s2){
-                intervals[i][0] = s1;
-                intervals[i][1] = Math.max(e1,e2);
+        });
+        List<List<Integer>> tempLs = new ArrayList<>();
+        List<Integer> tempIntLs = new ArrayList<>();
+        tempIntLs.add(intervals[0][0]);
+        tempIntLs.add(intervals[0][1]);
+        tempLs.add(tempIntLs);
+        for (int i=1 ; i<n ; i++){
+            int prevSt = tempLs.get(tempLs.size()-1).get(0);
+            int prevEnd = tempLs.get(tempLs.size()-1).get(1);
+            if (intervals[i][0]>prevEnd){
+                tempIntLs = new ArrayList<>();
+                tempIntLs.add(intervals[i][0]);
+                tempIntLs.add(intervals[i][1]);
+                tempLs.add(tempIntLs);
             }
             else{
-                ls.add(intervals[i-1]);
+                tempLs.get(tempLs.size()-1).set(0,prevSt);
+                tempLs.get(tempLs.size()-1).set(1,Math.max(prevEnd,intervals[i][1]));
+
             }
+
+	    }
+        int[][] ans = new int[tempLs.size()][2];
+        int idx = 0;
+        for (List<Integer> ls : tempLs){
+            ans[idx][0] = ls.get(0);
+            ans[idx][1] = ls.get(1);
+            idx++;
         }
-        ls.add(intervals[intervals.length-1]);
-        int[][] out = new int[ls.size()][2];
-        for (int i=0 ; i<ls.size() ; i++){
-            out[i][0] = ls.get(i)[0];
-            out[i][1] = ls.get(i)[1];
-        }
-        return out;
+        return ans;
     }
 }
