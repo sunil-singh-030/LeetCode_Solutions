c9d6d7eb4e71af9396eac9edcc9186a52f2dc317
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-25 08:04:15 +0530

    File updated at 10/25/2025, 8:04:11 AM

--- 1716-CalculateMoneyinLeetcodeBank/1716-CalculateMoneyinLeetcodeBank.java
+++ 1716-CalculateMoneyinLeetcodeBank/1716-CalculateMoneyinLeetcodeBank.java
@@ -1,19 +1,26 @@
-// Last updated: 10/25/2025, 8:02:37 AM
+// Last updated: 10/25/2025, 8:04:11 AM
 class Solution {
     public int totalMoney(int n) {
-        int tmoney = 0;
-        int extra = 0;
-        while (n>0){
-            if (n>=7){
-                tmoney += 28 + (extra*7);
-            }
-            else{
-                tmoney += (n*(n+1)/2) + extra*n;
-            }
-            n -= 7;
-            extra++;
-        }
-        return tmoney;
+        int res = 0;
+        int week = 1;
+        int day = 1;
+        int count = 1;
 
+        while(n != 0){
+            if(day < 8){
+                 res += count;
+            }else{
+                day = 1;
+                week++;
+                res += week;
+                count = week;
+            }
+
+            count++;
+            day++;
+            n--;
+        }
+
+        return res;
     }
 }
