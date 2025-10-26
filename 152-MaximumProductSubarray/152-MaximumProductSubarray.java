44497a7f6f592d8c5cc29dddb1d13f934b823d6c
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-26 21:57:38 +0530

    File updated at 10/26/2025, 9:57:38 PM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,15 +1,25 @@
-// Last updated: 8/1/2025, 7:10:36 AM
+// Last updated: 10/26/2025, 9:57:38 PM
 class Solution {
     public int maxProduct(int[] nums) {
-        int ans = Integer.MIN_VALUE;
         int n = nums.length;
+        int prevmin = 1;
+        int prevmax = 1;
+        int ans = Integer.MIN_VALUE;
         for (int i=0 ; i<n ; i++){
-            int prd = 1;
-            for (int j=i ; j<n ; j++){
-                prd *= nums[j];
-                ans = Math.max(ans,prd);
+            int num = nums[i];
+            int currMax = Math.max(num*prevmin,num*prevmax);
+            ans = Math.max(ans,currMax);
+            if (num>=0){
+                prevmax = Math.max(1,prevmax*num);
+                prevmin = Math.min(1,num*prevmin);
+            }
+            else{
+                int temp = prevmin;
+                prevmin = Math.min(1,prevmax*num);
+                prevmax = Math.max(1,temp*num);
             }
         }
         return ans;
     }
+    
 }
