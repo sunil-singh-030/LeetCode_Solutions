88d8cc4eafa8d54810660c278490b473120afe4c
Sunil Singh <sunil.singh.030250@gmail.com>
2025-10-30 10:23:53 +0530

    Create

---
+++ problem-1761800032172/problem-1761800032172.java
@@ -0,0 +1,10 @@
+// Last updated: 10/30/2025, 10:23:52 AM
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int min = target[0];
+        for (int i = 1; i < target.length; i++) {
+            min += Math.max(target[i] - target[i - 1], 0);
+        }
+        return min;
+    }
+}
