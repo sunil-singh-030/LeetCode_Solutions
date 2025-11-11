e2c2754ed866d871b84a28e1bb0b99b9bb7c0d4c
Sunil Singh <sunil.singh.030250@gmail.com>
2025-11-11 07:49:55 +0530

    File updated at 11/11/2025, 7:49:55 AM

--- 474-OnesandZeroes/474-OnesandZeroes.java
+++ 474-OnesandZeroes/474-OnesandZeroes.java
@@ -1,38 +1,70 @@
-// Last updated: 11/11/2025, 7:46:33 AM
+// Last updated: 11/11/2025, 7:49:55 AM
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int len = strs.length;
-        int[][] arr = new int[len][2];
-        for (int i=0 ; i<len ; i++){
-            String str = strs[i];
-            int cnt1 = 0;
-            for (char ch : str.toCharArray()){
-                if (ch=='1'){
-                    cnt1++;
+        int l=strs.length;
+        int[][] dp=new int[m+1][n+1];
+        dp[0][0]=1;
+        for(int i=1;i<=l;i++){
+            int one=0,zero=0;
+            String x=strs[i-1];
+            for(char c:x.toCharArray())if(c=='0')zero++;
+            one=x.length()-zero;
+            for(int j=m;j>=zero;j--){
+                for(int k=n;k>=one;k--){
+                    if(dp[j-zero][k-one]>0){
+                        dp[j][k]=Math.max(dp[j][k],1 + dp[j-zero][k-one]);
+                    }
                 }
             }
-            arr[i][0] = str.length() - cnt1;
-            arr[i][1] = cnt1;
         }
-        int[][][] dp = new int[len][m+1][n+1];
-        for (int[][] a  : dp){
-            for (int[] b : a){
-                Arrays.fill(b,-1);
+        int ans=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                ans=Math.max(ans,dp[i][j]);
             }
         }
-        return maxSize(arr,0,m,n,0,0,dp);
-    }
-    public int maxSize(int[][] arr, int idx, int m, int n, int cnt0, int cnt1, int[][][] dp){
-        if (idx==arr.length){
-            return 0;
-        }
-        if (dp[idx][cnt0][cnt1]!=-1){
-            return dp[idx][cnt0][cnt1];
-        }
-        int currSize = maxSize(arr,idx+1,m,n,cnt0,cnt1,dp);
-        if (arr[idx][0]+cnt0<=m && arr[idx][1]+cnt1<=n){
-            currSize = Math.max(currSize,1+maxSize(arr,idx+1,m,n,cnt0+arr[idx][0],cnt1+arr[idx][1],dp));
-        }
-        return dp[idx][cnt0][cnt1] = currSize;
+        return ans-1;
     }
 }
+
+// class Solution {
+//     public int findMaxForm(String[] strs, int m, int n) {
+        
+//         int len = strs.length;
+
+//         int[][] map = new int[len][2];
+//         for(int i=0; i<len; i++) {
+//             int l = strs[i].length();
+//             for(int j=0; j<l; j++) {
+//                 char ch = strs[i].charAt(j);
+//                 if(ch == '1') {
+//                     map[i][1]++;
+//                 }
+//                 else {
+//                     map[i][0]++;
+//                 }
+//             }
+//         }
+
+//         Integer[][][] dp = new Integer[len+1][m+1][n+1];
+//         return recur(map, 0, m, n, dp);
+//     }
+
+//     public int recur(int[][] map, int index, int m, int n, Integer[][][] dp) {
+
+//         if(m == 0 && n == 0) return dp[index][m][n] = 0;
+//         if(m < 0 || n < 0) return Integer.MIN_VALUE;
+//         if(index >= map.length) return dp[index][m][n] = 0;
+
+//         if(dp[index][m][n] != null) return dp[index][m][n];
+
+//         int pick = Integer.MIN_VALUE;
+//         if(m >= map[index][0] && n >= map[index][1]) {
+//             pick = 1 + recur(map, index + 1, m-map[index][0], n-map[index][1], dp);   
+//         }
+
+//         int noPick = recur(map, index + 1, m, n, dp);
+
+//         return dp[index][m][n] = Math.max(pick, noPick);
+//     }
+// }
