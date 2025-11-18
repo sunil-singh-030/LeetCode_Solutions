64f406d99e6ce3fa0177806ac2e4ff634ae94ea9
Sunil Singh <sunil.singh.030250@gmail.com>
2025-11-18 07:56:18 +0530

    File updated at 11/18/2025, 7:56:18 AM

--- 717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.java
+++ 717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.java
@@ -1,8 +1,7 @@
-// Last updated: 11/18/2025, 7:55:28 AM
+// Last updated: 11/18/2025, 7:56:18 AM
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int n = bits.length;
-        if (n==1) return true;
         int idx = 0;
         for (int i=0 ; i<n-1 ; i++){
             if (bits[i]==0){
