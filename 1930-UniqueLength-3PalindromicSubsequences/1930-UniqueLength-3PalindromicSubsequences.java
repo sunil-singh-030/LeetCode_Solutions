829386e4feb40964411a4322f1f6e5017c521b10
Sunil Singh <sunil.singh.030250@gmail.com>
2025-11-21 12:04:37 +0530

    File updated at 11/21/2025, 12:04:37 PM

--- 1930-UniqueLength-3PalindromicSubsequences/1930-UniqueLength-3PalindromicSubsequences.java
+++ 1930-UniqueLength-3PalindromicSubsequences/1930-UniqueLength-3PalindromicSubsequences.java
@@ -1,10 +1,9 @@
-// Last updated: 11/21/2025, 9:54:23 AM
+// Last updated: 11/21/2025, 12:04:37 PM
 class Solution {
     public int countPalindromicSubsequence(String s) {
         int[] stIdx = new int[26];
         int[] endIdx = new int[26];
         Arrays.fill(stIdx,-1);
-        Arrays.fill(endIdx,-1);
         int n = s.length();
         for (int i=0 ; i<n ; i++){
             char ch = s.charAt(i);
