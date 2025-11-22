79a830254d1ec1c17a88ec9da05a8625e13a53c7
Sunil Singh <sunil.singh.030250@gmail.com>
2025-11-22 19:37:13 +0530

    File updated at 11/22/2025, 7:37:12 PM

--- 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -1,11 +1,9 @@
-// Last updated: 11/22/2025, 7:36:23 PM
+// Last updated: 11/22/2025, 7:37:12 PM
 class Solution {
     public int minimumOperations(int[] nums) {
         int cnt = 0;
         for (int num : nums){
-            int way1 = num%3;
-            int way2 = 3 - way1;
-            cnt += Math.min(way1,way2);
+            cnt += Math.min(1,num%3);
         }
         return cnt;
     }
