ce38e88e67d430e6152c091e0d19a29d2b66377e
Sunil Singh <sunil.singh.030250@gmail.com>
2025-12-19 12:00:15 +0530

    Create 717-1BitAnd2BitCharacters

---
+++ 717-1BitAnd2BitCharacters/717-1BitAnd2BitCharacters.java
@@ -0,0 +1,21 @@
+// Last updated: 12/19/2025, 12:00:14 PM
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length;
+        int idx = 0;
+        for (int i=0 ; i<n-1 ; i++){
+            if (bits[i]==0){
+                idx = i+1;
+            }
+            else{
+                idx = i+2;
+                i++;
+            }
+        }
+        if (idx==n-1){
+            return true;
+        }
+        return false;
+        
+    }
+}
