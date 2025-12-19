a407b7a561b1db47a366dd8dce3130ad55fa3eec
Sunil Singh <sunil.singh.030250@gmail.com>
2025-12-19 11:58:59 +0530

    Create 1092-MaximumDifferenceBetweenNodeAndAncestor

---
+++ 1092-MaximumDifferenceBetweenNodeAndAncestor/1092-MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,37 @@
+// Last updated: 12/19/2025, 11:58:57 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return helper(root, root.val, root.val); 
+    }
+    
+    private int helper(TreeNode node, int curMax, int curMin) {
+        if (node == null) {
+            return curMax - curMin;
+        }
+        
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+        int left = helper(node.left, curMax, curMin);
+        int right = helper(node.right, curMax, curMin);
+        return Math.max(left, right);
+    }
+}
