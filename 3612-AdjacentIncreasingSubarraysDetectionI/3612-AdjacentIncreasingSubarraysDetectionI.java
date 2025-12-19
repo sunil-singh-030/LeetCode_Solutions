589aaa8f0a17fe4b980fcd4cae9b4b1fffe68798
Sunil Singh <sunil.singh.030250@gmail.com>
2025-12-19 11:55:58 +0530

    Create 3612-AdjacentIncreasingSubarraysDetectionI

---
+++ 3612-AdjacentIncreasingSubarraysDetectionI/3612-AdjacentIncreasingSubarraysDetectionI.java
@@ -0,0 +1,19 @@
+// Last updated: 12/19/2025, 11:55:57 AM
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        int[] cntBada = new int[n];
+        Arrays.fill(cntBada,1);
+        for (int i=n-2 ; i>=0 ; i--){
+            if (nums.get(i+1)>nums.get(i)){
+                cntBada[i] = cntBada[i+1] + 1;
+            }
+        }
+        for (int i=0 ; i<=n-2*k ; i++){
+            if (cntBada[i]>=k && cntBada[i+k]>=k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
