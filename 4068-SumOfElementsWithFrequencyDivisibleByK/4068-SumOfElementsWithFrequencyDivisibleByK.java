a7fc3af7e267d009224529e9da08356bfbd35490
Sunil Singh <sunil.singh.030250@gmail.com>
2025-12-19 11:54:17 +0530

    Create 4068-SumOfElementsWithFrequencyDivisibleByK

---
+++ 4068-SumOfElementsWithFrequencyDivisibleByK/4068-SumOfElementsWithFrequencyDivisibleByK.java
@@ -0,0 +1,16 @@
+// Last updated: 12/19/2025, 11:54:15 AM
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        int ans = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for (int key : map.keySet()){
+            if (map.get(key)%k==0){
+                ans += (map.get(key)*key);
+            }
+        }
+        return ans;
+    }
+}
