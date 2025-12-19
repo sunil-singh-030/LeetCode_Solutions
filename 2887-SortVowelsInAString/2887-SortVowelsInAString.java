95faa699ea0489a87ad9531e9665f7f9f78b28f5
Sunil Singh <sunil.singh.030250@gmail.com>
2025-12-19 11:56:29 +0530

    Create 2887-SortVowelsInAString

---
+++ 2887-SortVowelsInAString/2887-SortVowelsInAString.java
@@ -0,0 +1,34 @@
+// Last updated: 12/19/2025, 11:56:28 AM
+class Solution {
+    public String sortVowels(String s) {
+        int n = s.length();
+        char[] strArr = s.toCharArray();
+        HashSet<Character> set = new HashSet<>();
+        char[] arr = {'a','e','i','o','u','A','E','I','O','U'};
+        for (char ch : arr){
+            set.add(ch);
+        }
+        HashSet<Integer> indSet = new HashSet<>();
+        List<Character> ls = new ArrayList<>();
+        for (int i=0 ; i<n ; i++){
+            char ch = s.charAt(i);
+            if (set.contains(ch)){
+                indSet.add(i);
+                ls.add(ch);
+            }
+        }
+        Collections.sort(ls);
+        int ind = 0;
+        for (int i=0 ; i<n ; i++){
+            if (indSet.contains(i)){
+                strArr[i] = ls.get(ind);
+                ind++;
+            }
+        }
+        String ans = "";
+        for (char ch : strArr){
+            ans += ch;
+        }
+        return ans;
+    }
+}
