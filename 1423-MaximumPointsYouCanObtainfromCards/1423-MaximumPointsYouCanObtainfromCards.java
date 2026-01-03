b4a6fe73f51cd924b4289776ae1f7a2df0b7de50
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-03 16:07:25 +0530

    File updated at 1/3/2026, 4:07:24 PM

--- 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
+++ 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
@@ -1,27 +1,29 @@
-// Last updated: 1/3/2026, 4:00:39 PM
+// Last updated: 1/3/2026, 4:07:24 PM
 1class Solution {
 2    public int maxScore(int[] cardPoints, int k) {
 3        int n = cardPoints.length;
-4        int[] psum = new int[n+1];
-5        for (int i=0 ; i<n ; i++){
-6            psum[i+1] = psum[i] + cardPoints[i];
-7        }
-8        int sc = 0;
-9        int st = 0;
-10        int end = n-1;
-11        while (k>0){
-12            int lsum = psum[st+k]-psum[st];
-13            int rsum = psum[end+1]-psum[end-k+1];
-14            if (lsum>=rsum){
-15                sc += cardPoints[st];
-16                st++;
-17            }
-18            else{
-19                sc += cardPoints[end];
-20                end--;
-21            }
-22            k--;
+4        int total = 0;
+5        for (int x : cardPoints) total += x;
+6
+7        if (k == n) return total;
+8
+9        int windowSize = n - k;
+10        int curr = 0;
+11
+12        
+13        for (int i = 0; i < windowSize; i++) {
+14            curr += cardPoints[i];
+15        }
+16
+17        int minSum = curr;
+18
+19        for (int i = windowSize; i < n; i++) {
+20            curr += cardPoints[i];
+21            curr -= cardPoints[i - windowSize];
+22            minSum = Math.min(minSum, curr);
 23        }
-24        return sc;
-25    }
-26}
+24
+25        return total - minSum;
+26    }
+27}
+28
