7aa3dffcd6575d9bb0ed021734da306c2a2fdaa4
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-03 13:03:49 +0530

    File updated at 1/3/2026, 1:03:48 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,30 +1,42 @@
-// Last updated: 8/1/2025, 7:08:41 AM
-class Solution {
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        int n = nums.length;
-        for (int num : nums){
-            totalSum += num;
-        }
-        if (totalSum%2==1){
-            return false;
-        }
-        int k = totalSum/2;
-        int[] dp = new int[k+1];
-        dp[0] = 1;
-        for (int i=1 ; i<n ; i++){
-            int[] temp = dp.clone();
-            for (int j=1 ; j<=k ; j++){
-                boolean noTake = temp[j]==1;
-                boolean take = false;
-                if (j>=nums[i]){
-                    take = temp[j-nums[i]]==1;
-                }
-                dp[j] = (noTake || take ) ? 1 : 0;
-            }
-        }
-
-        return dp[k]==1;
-    }
-    
-}
+// Last updated: 1/3/2026, 1:03:48 PM
+1class Solution {
+2    public boolean canPartition(int[] nums) {
+3        int n = nums.length;
+4        int tsum = 0;
+5        for (int num : nums){
+6            tsum += num;
+7        }
+8        if (tsum%2==1){
+9            return false;
+10        }
+11        int[][] dp = new int[n][tsum/2+1];
+12        for (int[] a : dp){
+13            Arrays.fill(a,-1);
+14        }
+15        return helper(nums,0,tsum/2,dp);
+16    }
+17    public boolean helper(int[] nums, int idx, int sum, int[][] dp){
+18        if (sum==0){
+19            return true;
+20        }
+21        if (idx==nums.length){
+22            return false;
+23        }
+24        if (dp[idx][sum]!=-1){
+25            return dp[idx][sum]==1 ? true : false;
+26        }
+27        boolean notake = helper(nums,idx+1,sum,dp);
+28        boolean take = false;
+29        if (nums[idx]<=sum){
+30            take = helper(nums,idx+1,sum-nums[idx],dp);
+31        }
+32        if (notake||take==true){
+33            dp[idx][sum] = 1;
+34        }
+35        else{
+36            dp[idx][sum] = 0;
+37        }
+38        return notake || take;
+39    }
+40    
+41}
