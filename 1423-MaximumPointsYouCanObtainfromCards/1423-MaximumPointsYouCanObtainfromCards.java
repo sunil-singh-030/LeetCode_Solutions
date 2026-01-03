ecb3e3008c3e1885dd7171a39bd2eb10a93c96e4
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-03 16:00:41 +0530

    File updated at 1/3/2026, 4:00:39 PM

--- 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
+++ 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
@@ -1,28 +1,27 @@
-// Last updated: 1/3/2026, 3:59:57 PM
+// Last updated: 1/3/2026, 4:00:39 PM
 1class Solution {
 2    public int maxScore(int[] cardPoints, int k) {
-3        // 0 1 3 6 10 15 21 22
-4        int n = cardPoints.length;
-5        int[] psum = new int[n+1];
-6        for (int i=0 ; i<n ; i++){
-7            psum[i+1] = psum[i] + cardPoints[i];
-8        }
-9        int sc = 0;
-10        int st = 0;
-11        int end = n-1;
-12        while (k>0){
-13            int lsum = psum[st+k]-psum[st];
-14            int rsum = psum[end+1]-psum[end-k+1];
-15            if (lsum>=rsum){
-16                sc += cardPoints[st];
-17                st++;
-18            }
-19            else{
-20                sc += cardPoints[end];
-21                end--;
-22            }
-23            k--;
-24        }
-25        return sc;
-26    }
-27}
+3        int n = cardPoints.length;
+4        int[] psum = new int[n+1];
+5        for (int i=0 ; i<n ; i++){
+6            psum[i+1] = psum[i] + cardPoints[i];
+7        }
+8        int sc = 0;
+9        int st = 0;
+10        int end = n-1;
+11        while (k>0){
+12            int lsum = psum[st+k]-psum[st];
+13            int rsum = psum[end+1]-psum[end-k+1];
+14            if (lsum>=rsum){
+15                sc += cardPoints[st];
+16                st++;
+17            }
+18            else{
+19                sc += cardPoints[end];
+20                end--;
+21            }
+22            k--;
+23        }
+24        return sc;
+25    }
+26}
