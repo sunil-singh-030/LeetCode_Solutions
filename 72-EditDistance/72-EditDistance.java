a944df5edbef0f957ea62fcb024ea05326f09b83
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-04 12:35:03 +0530

    File updated at 1/4/2026, 12:35:02 PM

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,4 +1,4 @@
-// Last updated: 1/4/2026, 12:33:37 PM
+// Last updated: 1/4/2026, 12:35:02 PM
 1class Solution {
 2    public int minDistance(String word1, String word2) {
 3        int m = word1.length();
@@ -9,48 +9,46 @@
 8            prev[j] = j;
 9        }
 10        for (int i=1 ; i<=m ; i++){
-11            for (int j=0 ; j<=n ; j++){
-12                if (j==0){
-13                    curr[j] = i;
-14                    continue;
-15                }
-16                char ch1 = word1.charAt(i-1);
-17                char ch2 = word2.charAt(j-1);
-18                if (ch1==ch2){
-19                    curr[j] = prev[j-1];
-20                }
-21                else{
-22                    int insert = 1 + curr[j-1];
-23                    int del = 1 + prev[j];
-24                    int repl = 1 + prev[j-1];
-25                    curr[j] = Math.min(insert,Math.min(del,repl));
-26                }
-27            }
-28            prev = curr.clone();
-29        }
-30        return prev[n];
-31    }
-32    // public int helper(String s1, String s2, int i, int j, int[][] dp){
-33    //     if (i<0){
-34    //         return j+1;
-35    //     }
-36    //     if (j<0){
-37    //         return i+1;
-38    //     }
-39    //     if (dp[i+1][j+1]!=-1){
-40    //         return dp[i+1][j+1];
-41    //     }
-42    //     char ch1 = s1.charAt(i);
-43    //     char ch2 = s2.charAt(j);
-44
-45    //     if (ch1==ch2){
-46    //         return dp[i+1][j+1] = helper(s1,s2,i-1,j-1,dp);
-47    //     }
-48    //     else{
-49    //         int insert = 1 + helper(s1,s2,i,j-1,dp);
-50    //         int del = 1 + helper(s1,s2,i-1,j,dp);
-51    //         int repl = 1 + helper(s1,s2,i-1,j-1,dp);
-52    //         return dp[i+1][j+1] = Math.min(insert,Math.min(del,repl));
-53    //     }
-54    // }
-55}
+11            curr[0] = i;
+12            for (int j=1 ; j<=n ; j++){
+13                
+14                char ch1 = word1.charAt(i-1);
+15                char ch2 = word2.charAt(j-1);
+16                if (ch1==ch2){
+17                    curr[j] = prev[j-1];
+18                }
+19                else{
+20                    int insert = 1 + curr[j-1];
+21                    int del = 1 + prev[j];
+22                    int repl = 1 + prev[j-1];
+23                    curr[j] = Math.min(insert,Math.min(del,repl));
+24                }
+25            }
+26            prev = curr.clone();
+27        }
+28        return prev[n];
+29    }
+30    // public int helper(String s1, String s2, int i, int j, int[][] dp){
+31    //     if (i<0){
+32    //         return j+1;
+33    //     }
+34    //     if (j<0){
+35    //         return i+1;
+36    //     }
+37    //     if (dp[i+1][j+1]!=-1){
+38    //         return dp[i+1][j+1];
+39    //     }
+40    //     char ch1 = s1.charAt(i);
+41    //     char ch2 = s2.charAt(j);
+42
+43    //     if (ch1==ch2){
+44    //         return dp[i+1][j+1] = helper(s1,s2,i-1,j-1,dp);
+45    //     }
+46    //     else{
+47    //         int insert = 1 + helper(s1,s2,i,j-1,dp);
+48    //         int del = 1 + helper(s1,s2,i-1,j,dp);
+49    //         int repl = 1 + helper(s1,s2,i-1,j-1,dp);
+50    //         return dp[i+1][j+1] = Math.min(insert,Math.min(del,repl));
+51    //     }
+52    // }
+53}
