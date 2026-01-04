36f308b6e2a2de644421a03a26ae6a3c6b04a3bb
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-04 15:03:11 +0530

    File updated at 1/4/2026, 3:03:10 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,19 +1,26 @@
-// Last updated: 10/5/2025, 7:22:43 AM
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
-        for (int i=m-1 ; i>=0 ; i--){
-            for (int j=n-1 ; j>=0 ; j--){
-                if (j!=n-1){
-                    dp[j] = dp[j+1] + dp[j];
-                    continue;
-                }
-                if (i==m-1 && j==n-1){
-                    dp[j] = 1;
-                    continue;
-                }
-            }
-        }
-        return dp[0];
-    }
-}
+// Last updated: 1/4/2026, 3:03:10 PM
+1class Solution {
+2    public int uniquePaths(int m, int n) {
+3        int[][] dp = new int[m][n];
+4        for (int[] a : dp){
+5            Arrays.fill(a,-1);
+6        }
+7        return helper(m,n,0,0,dp);
+8    }
+9    public int helper(int m, int n, int r, int c, int[][] dp){
+10        if (r==m-1 && c==n-1){
+11            return 1;
+12        }
+13        if (dp[r][c]!=-1){
+14            return dp[r][c];
+15        }
+16        int cnt = 0;
+17        if (r+1<m){
+18            cnt += helper(m,n,r+1,c,dp);
+19        }
+20        if (c+1<n){
+21            cnt += helper(m,n,r,c+1,dp);
+22        }
+23        return dp[r][c] = cnt;
+24    }
+25}
