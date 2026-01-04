1d468e4a16c64068a3ce0eabe6c3e81cee592009
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-04 10:47:18 +0530

    File updated at 1/4/2026, 10:47:17 AM

--- 1390-FourDivisors/1390-FourDivisors.java
+++ 1390-FourDivisors/1390-FourDivisors.java
@@ -1,23 +1,36 @@
-// Last updated: 1/4/2026, 10:43:22 AM
-1class Solution {
-2    public int sumFourDivisors(int[] nums) {
-3        int ans = 0;
-4        for (int num : nums){
-5            int cdiv = 0;
-6            int sum = num+1;
-7            for (int i=2 ; i<num ; i++){
-8                if (num%i==0){
-9                    sum += i;
-10                    cdiv++;
-11                }
-12                if (cdiv==3){
-13                    break;
-14                }
-15            }
-16            if (cdiv==2){
-17                ans += sum;
-18            }
-19        }
-20        return ans;
-21    }
-22}
+// Last updated: 1/4/2026, 10:47:17 AM
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+        
+        for (int n : nums) {
+            int count = 0;
+            int currentSum = 0;
+            
+            // Iterate up to the square root of n
+            for (int d = 1; d * d <= n; d++) {
+                if (n % d == 0) {
+                    // d is a divisor
+                    count++;
+                    currentSum += d;
+                    
+                    // If d is not the square root, add its counterpart n/d
+                    if (d * d != n) {
+                        count++;
+                        currentSum += n / d;
+                    }
+                }
+                
+                // Optimization: if we already have more than 4, stop
+                if (count > 4) break;
+            }
+            
+            // If exactly four divisors were found, add their sum to total
+            if (count == 4) {
+                totalSum += currentSum;
+            }
+        }
+        
+        return totalSum;
+    }
+}
