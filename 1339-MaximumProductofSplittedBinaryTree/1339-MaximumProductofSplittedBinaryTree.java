a4c6128e8cfc23e2e2d09361b2590b0bc8a508ba
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-07 19:42:41 +0530

    File updated at 1/7/2026, 7:42:39 PM

--- 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.java
+++ 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.java
@@ -1,11 +1,11 @@
-// Last updated: 1/7/2026, 7:42:29 PM
+// Last updated: 1/7/2026, 7:42:39 PM
 1class Solution {
 2    long total = 0, max = 0;
 3    static final int MOD = 1_000_000_007;
 4
 5    public int maxProduct(TreeNode root) {
-6        total = sum(root);   // total tree sum
-7        dfs(root);           // compute max product
+6        total = sum(root);   
+7        dfs(root);           
 8        return (int)(max % MOD);
 9    }
 10
