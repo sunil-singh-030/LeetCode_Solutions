9c91c7d6c079fb5d06f21b13c250dc0373d13ae6
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-08 11:46:02 +0530

    Create 2320-CountNumberofWaystoPlaceHouses

---
+++ 2320-CountNumberofWaystoPlaceHouses/2320-CountNumberofWaystoPlaceHouses.java
@@ -0,0 +1,18 @@
+// Last updated: 1/8/2026, 11:46:02 AM
+1class Solution {
+2    public int countHousePlacements(int n) {
+3        int MOD = 1_000_000_007;
+4
+5        long a = 1;
+6        long b = 2;
+7
+8        for (int i = 2; i <= n; i++) {
+9            long c = (a + b) % MOD;
+10            a = b;
+11            b = c;
+12        }
+13
+14        return (int)((b * b) % MOD);
+15    }
+16}
+17
