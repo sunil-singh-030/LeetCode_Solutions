fb860b6e9dc0133a45997f879329a21fb5cb3664
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-10 18:29:25 +0530

    File updated at 1/10/2026, 6:29:24 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,30 +1,56 @@
-// Last updated: 1/10/2026, 6:28:58 PM
-1class Solution {
-2    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-3        ListNode t1 = l1, t2 = l2;
-4        int carry = 0;
-5
-6        ListNode ans = new ListNode(0); 
-7        ListNode temp = ans;
-8
-9        while (t1 != null || t2 != null || carry > 0) {
-10            int sum = carry;
-11
-12            if (t1 != null) {
-13                sum += t1.val;
-14                t1 = t1.next;
-15            }
-16            if (t2 != null) {
-17                sum += t2.val;
-18                t2 = t2.next;
-19            }
-20
-21            temp.next = new ListNode(sum % 10); 
-22            carry = sum / 10;
-23            temp = temp.next;
-24        }
-25
-26        return ans.next; 
-27    }
-28}
-29
+// Last updated: 1/10/2026, 6:29:24 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+13        ListNode t1 = l1;
+14        ListNode t2 = l2;
+15        int carry = 0;
+16        ListNode ans = new ListNode();
+17        ListNode temp = ans;
+18        while (t1!=null && t2!=null){
+19            temp.next = new ListNode();
+20            temp = temp.next;
+21            int num = t1.val + t2.val + carry;
+22            temp.val = num%10;
+23            carry = num/10;
+24            t1 = t1.next;
+25            t2 = t2.next;
+26            
+27        }
+28        while (t1!=null){
+29            temp.next = new ListNode();
+30            temp = temp.next;
+31            int num = t1.val + carry;
+32            temp.val = num%10;
+33            carry = num/10;
+34            t1 = t1.next;
+35            
+36        }
+37        while (t2!=null){
+38            temp.next = new ListNode();
+39            temp = temp.next;
+40            int num = t2.val + carry;
+41            temp.val = num%10;
+42            carry = num/10;
+43            t2 = t2.next;
+44            
+45        }
+46        while (carry>0){
+47            temp.next = new ListNode();
+48            temp = temp.next;
+49            temp.val = carry%10;
+50            carry /= 10;
+51            
+52        }
+53        return ans.next;
+54    }
+55}
