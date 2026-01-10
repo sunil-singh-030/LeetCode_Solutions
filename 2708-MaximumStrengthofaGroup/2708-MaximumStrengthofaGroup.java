415a8305c8dd536e97dd6b269fe9450be7925e01
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-10 13:01:44 +0530

    File updated at 1/10/2026, 1:01:44 PM

--- 2708-MaximumStrengthofaGroup/2708-MaximumStrengthofaGroup.java
+++ 2708-MaximumStrengthofaGroup/2708-MaximumStrengthofaGroup.java
@@ -1,26 +1,28 @@
-// Last updated: 1/10/2026, 12:50:35 PM
+// Last updated: 1/10/2026, 1:01:44 PM
 1class Solution {
 2    public long maxStrength(int[] nums) {
 3        int n = nums.length;
-4        boolean[] visited = new boolean[n];
-5        return helper(nums,0,visited);
-6    }
-7    public long helper(int[] nums, int i, boolean[] visited){
-8        if (i==nums.length){
-9            boolean check = false;
-10            long prd = 1;
-11            for (int idx=0 ; idx<nums.length ; idx++){
-12                if (visited[idx]){
-13                    prd *= (long) nums[idx];
-14                    check = true;
-15                }
-16            }
-17            return check ? prd : Long.MIN_VALUE;
-18        }
-19        visited[i] = true;
-20        long take = helper(nums,i+1,visited);
-21        visited[i] = false;
-22        long notake = helper(nums,i+1,visited);
-23        return Math.max(notake,take);
-24    }
-25}
+4        long[][] dp = new long[n+1][2];
+5        dp[0][0] = dp[0][1] = 1;
+6        long max = Long.MIN_VALUE;
+7        for (int num : nums){
+8            max = Math.max(max,(long) num);
+9        }
+10        boolean check = false;
+11        for (int i=0 ; i<n ; i++){
+12            long num = (long) nums[i];
+13            long currMin = Math.min(dp[i][0]*num,dp[i][1]*num);
+14            long currMax = Math.max(dp[i][0]*num,dp[i][1]*num);
+15            if (currMax==1){
+16                check = true;
+17            }
+18            dp[i+1][0] = Math.min(dp[i][0],currMin);
+19            dp[i+1][1] = Math.max(dp[i][1],currMax);
+20        }
+21        if (dp[n][1]!=1){
+22            return dp[n][1];
+23        }
+24        return check ? 1 : max;
+25    }
+26    
+27}
