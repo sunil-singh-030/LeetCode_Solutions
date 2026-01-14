ba49819ce71cbe6ba4e0951ae3ea8846035fb649
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-14 12:42:39 +0530

    File updated at 1/14/2026, 12:42:39 PM

--- 260-SingleNumberIII/260-SingleNumberIII.java
+++ 260-SingleNumberIII/260-SingleNumberIII.java
@@ -1,11 +1,11 @@
-// Last updated: 1/14/2026, 12:41:47 PM
+// Last updated: 1/14/2026, 12:42:39 PM
 1class Solution {
 2    public int[] singleNumber(int[] nums) {
 3        int xor = 0;
 4        for (int num : nums){
 5            xor ^= num;
 6        }
-7        int req = xor & (~(xor-1));
+7        int req = xor & (-xor);
 8        int n1 = 0;
 9        for (int num : nums){
 10            if ((req&num)==0){
