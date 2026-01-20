ae942c9752c73285f6984544757bb4c892e74d80
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-20 11:21:29 +0530

    File updated at 1/20/2026, 11:21:28 AM

--- 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
+++ 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
@@ -1,48 +1,43 @@
-// Last updated: 1/20/2026, 11:19:27 AM
-1class Solution {
-2    public int maximumLength(String s) {
-3        int n = s.length();
-4        int maxLen = n+1;
-5        int st = 1;
-6        int end = n-2;
-7        while (st<=end){
-8            int mid = (st+end)/2;
-9            if (jaiMataDi(s,mid)){
-10                maxLen = mid;
-11                st = mid+1;
-12            }
-13            else{
-14                end = mid-1;
-15            }
-16        }
-17        return maxLen==n+1 ? -1 : maxLen;
-18    }
-19    public boolean jaiMataDi(String s, int len){
-20        HashMap<String,Integer> map = new HashMap<>();
-21        HashMap<Character,Integer> freq = new HashMap<>();
-22        for (int i=0 ; i<len ; i++){
-23            char ch = s.charAt(i);
-24            freq.put(ch,freq.getOrDefault(ch,0)+1);
-25        }
-26        if (freq.size()==1){
-27            map.put(s.substring(0,len),1);
-28        }
-29        for (int i=len ; i<s.length() ; i++){
-30            char ch = s.charAt(i);
-31            freq.put(ch,freq.getOrDefault(ch,0)+1);
-32            freq.put(s.charAt(i-len),freq.get(s.charAt(i-len))-1);
-33            if (freq.get(s.charAt(i-len))==0){
-34                freq.remove(s.charAt(i-len));
-35            }
-36            if (freq.size()==1){
-37                String curr = s.substring(i+1-len,i+1);
-38                int prevCnt = map.getOrDefault(curr,0);
-39                if (prevCnt==2){
-40                    return true;
-41                }
-42                map.put(curr,prevCnt+1);
-43            }    
-44        }
-45        return false;
-46    }
-47}
+// Last updated: 1/20/2026, 11:21:28 AM
+class Solution {
+    public int maximumLength(String s) {
+        int n = s.length();
+        int[][] top = new int[26][3];
+        int[] total = new int[26];
+
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && s.charAt(j) == s.charAt(i)) j++;
+            int len = j - i;
+            int c = s.charAt(i) - 'a';
+
+            total[c] += len;
+
+            if (len > top[c][0]) {
+                top[c][2] = top[c][1];
+                top[c][1] = top[c][0];
+                top[c][0] = len;
+            } else if (len > top[c][1]) {
+                top[c][2] = top[c][1];
+                top[c][1] = len;
+            } else if (len > top[c][2]) {
+                top[c][2] = len;
+            }
+
+            i = j;
+        }
+
+        int ans = -1;
+
+        for (int c = 0; c < 26; c++) {
+            int a = top[c][0], b = top[c][1], d = top[c][2];
+
+            if (a >= 3) ans = Math.max(ans, a - 2);
+            if (b > 0) ans = Math.max(ans, (a + b - 1) / 2);
+            if (d > 0) ans = Math.max(ans, Math.min(a, Math.min(b, d)));
+            if (total[c] >= 3) ans = Math.max(ans, 1);
+        }
+
+        return ans <= 0 ? -1 : ans;
+    }
+}
