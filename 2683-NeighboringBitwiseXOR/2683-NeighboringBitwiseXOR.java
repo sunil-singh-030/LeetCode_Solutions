c983b5112fdc9e8710d8a785be2523f948735192
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-21 11:52:06 +0530

    File updated at 1/21/2026, 11:52:04 AM

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,17 +1,17 @@
-// Last updated: 1/21/2026, 11:46:18 AM
+// Last updated: 1/21/2026, 11:52:04 AM
 1class Solution {
 2    public int xorAllNums(int[] nums1, int[] nums2) {
 3        int exor1 = 0;;
 4        if (nums2.length%2==1){
-5            for (int i=0;i<nums1.length;i++){
-6                exor1^=nums1[i];
+5            for (int num : nums1){
+6                exor1^=num;
 7            }
 8        }
 9        
 10        int exor2 = 0;
 11        if (nums1.length%2==1){
-12            for (int i=0;i<nums2.length;i++){
-13                exor2^=nums2[i];
+12            for (int num : nums2){
+13                exor2^=num;
 14            }
 15        }
 16        return exor1^exor2;
