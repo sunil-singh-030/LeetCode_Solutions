0c694ed016ea504fee7a3accd91a857432048d5c
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-21 12:07:33 +0530

    File updated at 1/21/2026, 12:07:31 PM

--- 1835-FindXORSumofAllPairsBitwiseAND/1835-FindXORSumofAllPairsBitwiseAND.java
+++ 1835-FindXORSumofAllPairsBitwiseAND/1835-FindXORSumofAllPairsBitwiseAND.java
@@ -1,4 +1,4 @@
-// Last updated: 1/21/2026, 12:07:11 PM
+// Last updated: 1/21/2026, 12:07:31 PM
 1class Solution {
 2    public int getXORSum(int[] arr1, int[] arr2) {
 3        int xor2 = 0;
@@ -7,9 +7,8 @@
 6        } 
 7        int ans = 0;
 8        for (int num : arr1){
-9            int curr = num&xor2;
-10            ans ^= curr;
-11        }
-12        return ans;
-13    }
-14}
+9            ans ^= num&xor2;
+10        }
+11        return ans;
+12    }
+13}
