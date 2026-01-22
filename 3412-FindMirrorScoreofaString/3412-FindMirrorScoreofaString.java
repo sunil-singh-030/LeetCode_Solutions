cf69e4b9162108b7a6d68b4063b380c050ede0d8
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-22 14:31:16 +0530

    File updated at 1/22/2026, 2:31:16 PM

--- 3412-FindMirrorScoreofaString/3412-FindMirrorScoreofaString.java
+++ 3412-FindMirrorScoreofaString/3412-FindMirrorScoreofaString.java
@@ -1,27 +1,20 @@
-// Last updated: 1/22/2026, 2:27:41 PM
-1class Solution {
-2    public long calculateScore(String s) {
-3        long ans = 0;
-4        List<List<Integer>> ls = new ArrayList<>();
-5        for (int i=0 ; i<26 ; i++){
-6            ls.add(new ArrayList<>());
-7        }
-8        int n = s.length();
-9        for (int i=0 ; i<n ; i++){
-10            int asci = s.charAt(i)-'a';
-11            int req = 25-asci;
-12            if (ls.get(req).size()>0){
-13                int size = ls.get(req).size();
-14                int recent = ls.get(req).get(size-1);
-15                ls.get(req).remove(size-1);
-16                ans += i-recent;
-17            }
-18            else{
-19                ls.get(asci).add(i);
-20            }
-21            //System.out.println(ans);
-22
-23        }
-24        return ans;
-25    }
-26}
+// Last updated: 1/22/2026, 2:31:16 PM
+class Solution {
+    public long calculateScore(String s) {
+        HashMap<Character,Stack<Integer>> map = new HashMap<>();
+        long target = 0;
+
+        for(int i = 0; i< s.length();i++){
+            int dis = s.charAt(i)- 'a';
+            char mirror = (char)('z' - dis);
+
+            if(map.containsKey(mirror)&& !map.get(mirror).isEmpty()){
+              target  += (i - map.get(mirror).pop());
+            }else{
+                map.computeIfAbsent(s.charAt(i),k -> new Stack<>()).push(i);
+            }
+        }
+        return target;
+        
+    }
+}
