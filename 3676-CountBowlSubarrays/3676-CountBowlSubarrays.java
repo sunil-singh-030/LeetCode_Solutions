d945d6fe223148542b4ceaec9ed25ac27a8e695c
Sunil Singh <sunil.singh.030250@gmail.com>
2026-01-27 15:28:07 +0530

    File updated at 1/27/2026, 3:28:06 PM

--- 3676-CountBowlSubarrays/3676-CountBowlSubarrays.java
+++ 3676-CountBowlSubarrays/3676-CountBowlSubarrays.java
@@ -1,4 +1,4 @@
-// Last updated: 1/27/2026, 3:25:23 PM
+// Last updated: 1/27/2026, 3:28:06 PM
 1class Solution {
 2    public long bowlSubarrays(int[] nums) {
 3        long ans = 0;
@@ -7,15 +7,14 @@
 6        for (int i=1 ; i<n ; i++){
 7            lmax[i] = Math.max(lmax[i-1],nums[i-1]);
 8        }
-9        int[] rmax = new int[n];
-10        for (int i=n-2 ; i>=0 ; i--){
-11            rmax[i] = Math.max(rmax[i+1],nums[i+1]);
-12        }
-13        for (int i=0 ; i<n ; i++){
-14            if (lmax[i]>nums[i] && rmax[i]>nums[i]){
-15                ans++;
-16            }
-17        }
-18        return ans;
-19    }
-20}
+9        int rmax = 0;
+10        for (int i=n-2 ; i>0 ; i--){
+11            rmax = Math.max(rmax,nums[i+1]);
+12            if (lmax[i]>nums[i] && rmax>nums[i]){
+13                ans++;
+14            }
+15        }
+16        
+17        return ans;
+18    }
+19}
