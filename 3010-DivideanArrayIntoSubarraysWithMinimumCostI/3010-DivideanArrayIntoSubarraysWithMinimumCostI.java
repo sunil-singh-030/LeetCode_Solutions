1c5057b14d52dc339d30b237354a12a4da63f4bb
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-01 07:59:23 +0530

    File updated at 2/1/2026, 7:59:22 AM

--- 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.java
+++ 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.java
@@ -1,13 +1,20 @@
-// Last updated: 2/1/2026, 7:53:47 AM
+// Last updated: 2/1/2026, 7:59:22 AM
 1class Solution {
 2    public int minimumCost(int[] nums) {
 3        int ans = Integer.MAX_VALUE;
 4        int n = nums.length;
-5        for (int i=1 ; i<n-1 ; i++){
-6            for (int j=i+1 ; j<n ; j++){
-7                ans = Math.min(ans,nums[0]+nums[i]+nums[j]);
-8            }
-9        }
-10        return ans;
-11    }
-12}
+5        int min1 = Integer.MAX_VALUE;
+6        int min2 = Integer.MAX_VALUE;
+7        for (int i=1 ; i<n ; i++){
+8            if (nums[i]<min1){
+9                min2 = Math.min(min2,min1);
+10                min1 = nums[i];
+11            }
+12            else{
+13                min2 = Math.min(min2,nums[i]);
+14            }
+15            
+16        }
+17        return nums[0]+min1+min2;
+18    }
+19}
