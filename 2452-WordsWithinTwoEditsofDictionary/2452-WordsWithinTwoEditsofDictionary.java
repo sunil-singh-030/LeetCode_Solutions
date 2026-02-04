28ab5fe20ad66971ebc31aabccd5a874c7247a24
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-04 15:06:58 +0530

    File updated at 2/4/2026, 3:06:58 PM

--- 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
+++ 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
@@ -1,4 +1,4 @@
-// Last updated: 2/4/2026, 3:03:46 PM
+// Last updated: 2/4/2026, 3:06:58 PM
 1class TrieNode{
 2    TrieNode[] child;
 3    boolean isEnd;
@@ -23,47 +23,48 @@
 22        }
 23        curr.isEnd = true;
 24    }
-25    public boolean check(String s, int chance, TrieNode curr){
-26        if (s.isEmpty()){
-27            return curr.isEnd;
+25    public boolean check(String s, int idx, int chance, TrieNode curr){
+26        if (curr==null){
+27            curr = root;
 28        }
-29        if (curr==null){
-30            curr = root;
+29        if (idx==s.length()){
+30            return curr.isEnd;
 31        }
-32        boolean res = false;
-33        if (curr.child[s.charAt(0)-'a']!=null){
-34            res = res || check(s.substring(1),chance,curr.child[s.charAt(0)-'a']);
-35        }
-36        if (res){
-37            return true;
-38        }
-39        if (chance>0){
-40            for (int i=0 ; i<26 ; i++){
-41                if (curr.child[i]!=null){
-42                    res = res || check(s.substring(1),chance-1,curr.child[i]);
-43                    if (res){
-44                    return true;
-45                }
+32        
+33        boolean res = false;
+34        if (curr.child[s.charAt(idx)-'a']!=null){
+35            res = res || check(s,idx+1,chance,curr.child[s.charAt(idx)-'a']);
+36        }
+37        if (res){
+38            return true;
+39        }
+40        if (chance>0){
+41            for (int i=0 ; i<26 ; i++){
+42                if (curr.child[i]!=null){
+43                    res = res || check(s,idx+1,chance-1,curr.child[i]);
+44                    if (res){
+45                    return true;
 46                }
-47            }
-48        }
-49        
-50        return false;
-51
-52    }
-53}
-54class Solution {
-55    public List<String> twoEditWords(String[] queries, String[] dictionary) {
-56        Trie t = new Trie();
-57        for (String s : dictionary){
-58            t.insert(s);
-59        }
-60        List<String> ans = new ArrayList<>();
-61        for (String q : queries){
-62            if (t.check(q,2,null)){
-63                ans.add(q);
-64            }
-65        }
-66        return ans;
-67    }
-68}
+47                }
+48            }
+49        }
+50        
+51        return false;
+52
+53    }
+54}
+55class Solution {
+56    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+57        Trie t = new Trie();
+58        for (String s : dictionary){
+59            t.insert(s);
+60        }
+61        List<String> ans = new ArrayList<>();
+62        for (String q : queries){
+63            if (t.check(q,0,2,null)){
+64                ans.add(q);
+65            }
+66        }
+67        return ans;
+68    }
+69}
