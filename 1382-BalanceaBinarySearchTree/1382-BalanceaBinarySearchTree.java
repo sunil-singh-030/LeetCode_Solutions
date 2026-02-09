2e445fe4bc0ac2dc7b9726399f17f13d9f8d5514
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-09 11:06:06 +0530

    File updated at 2/9/2026, 11:06:06 AM

--- 1382-BalanceaBinarySearchTree/1382-BalanceaBinarySearchTree.java
+++ 1382-BalanceaBinarySearchTree/1382-BalanceaBinarySearchTree.java
@@ -1,47 +1,39 @@
-// Last updated: 2/9/2026, 10:51:26 AM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public TreeNode balanceBST(TreeNode root) {
-18        List<Integer> ls = new ArrayList<>();
-19        TreeNode temp = root;
-20        Stack<TreeNode> st = new Stack<>();
-21        while (temp!=null || !st.isEmpty()){
-22            while (temp!=null){
-23                st.push(temp);
-24                temp = temp.left;
-25            }
-26            TreeNode curr = st.pop();
-27            ls.add(curr.val);
-28            if (curr.right!=null){
-29                temp = curr.right;
-30            }
-31        }
-32        
-33        return jaiBabaKi(ls,0,ls.size()-1);
-34    }
-35    public TreeNode jaiBabaKi(List<Integer> ls, int st, int end){
-36        if (st>end){
-37            return null;
-38        }
-39        int idx = (st+end)/2;
-40        TreeNode newNode = new TreeNode(ls.get(idx));
-41        newNode.left = jaiBabaKi(ls,st,idx-1);
-42        newNode.right = jaiBabaKi(ls,idx+1,end);
-43        return newNode;
-44    }
-45    
-46}
+// Last updated: 2/9/2026, 11:06:06 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<TreeNode> res = new ArrayList<TreeNode>();
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return Build_BST(0, res.size() - 1);
+    }
+
+    public void inorder(TreeNode root ){
+         if(root == null) return;
+         inorder(root.left);
+         res.add(root);
+         inorder(root.right);
+    }
+    
+    public TreeNode Build_BST( int start, int end){
+        if(start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode node = res.get(mid);
+        node.left = Build_BST( start, mid - 1);
+        node.right = Build_BST( mid + 1, end);
+        return node;
+    }
+}
