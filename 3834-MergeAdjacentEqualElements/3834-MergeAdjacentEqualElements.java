c012f4a7c7078b84bc68f8814205e602548704b2
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-09 12:23:29 +0530

    File updated at 2/9/2026, 12:23:28 PM

--- 3834-MergeAdjacentEqualElements/3834-MergeAdjacentEqualElements.java
+++ 3834-MergeAdjacentEqualElements/3834-MergeAdjacentEqualElements.java
@@ -1,20 +1,16 @@
-// Last updated: 2/9/2026, 12:20:00 PM
+// Last updated: 2/9/2026, 12:23:28 PM
 1class Solution {
-2    public List<Long> mergeAdjacent(int[] nums) {
-3        Stack<Long> st = new Stack<>();
-4        for (int num : nums){
-5            long temp = (long) num;
-6            while (!st.isEmpty() && st.peek()==temp){
-7                st.pop();
-8                temp *= 2;
-9            }
-10            st.push(temp);
-11        }
-12        List<Long> ans = new ArrayList<>();
-13        while (!st.isEmpty()){
-14            ans.add(st.pop());
-15        }
-16        Collections.reverse(ans);
-17        return ans;
-18    }
-19}
+2    public int dominantIndices(int[] nums) {
+3        int cnt = 0;
+4        int n  = nums.length;
+5        int sum = nums[n-1];
+6        for (int i=n-2 ; i>=0 ; i--){
+7            int len = n-1-i;
+8            if (nums[i]>(sum/len)){
+9                cnt++;
+10            }
+11            sum += nums[i];
+12        }
+13        return cnt;
+14    }
+15}
