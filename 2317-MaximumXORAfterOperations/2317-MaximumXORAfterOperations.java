1aff36c49433e5e26de96bbf9123c4ef95813b0f
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-11 12:38:00 +0530

    File updated at 2/11/2026, 12:38:00 PM

--- 2317-MaximumXORAfterOperations/2317-MaximumXORAfterOperations.java
+++ 2317-MaximumXORAfterOperations/2317-MaximumXORAfterOperations.java
@@ -1,21 +1,12 @@
-// Last updated: 2/11/2026, 12:36:31 PM
-1class Solution {
-2    public int maximumXOR(int[] nums) {
-3        int ans = 0;
-4        for (int i=0 ; i<32 ; i++){
-5            int cnt1 = 0;
-6            for (int j=0 ; j<nums.length ; j++){
-7                if (nums[j]>0){
-8                    cnt1 += nums[j]%2==1 ? 1 : 0;
-9                    nums[j] /= 2;
-10                }
-11                
-12            }
-13            if (cnt1>0){
-14                ans += (int) Math.pow(2,i);
-15            }
-16            
-17        }
-18        return ans;
-19    }
-20}
+// Last updated: 2/11/2026, 12:38:00 PM
+class Solution {
+    public int maximumXOR(int[] nums) {
+        int ans=0;
+        for(int n:nums)
+        {
+            ans|=n;
+        }
+        return ans;
+        
+    }
+}
