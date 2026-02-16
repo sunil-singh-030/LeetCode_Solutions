ac434eaece10e82fb0c2b0fbd9627180cd01636c
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-16 10:07:34 +0530

    File updated at 2/16/2026, 10:07:33 AM

--- 190-ReverseBits/190-ReverseBits.java
+++ 190-ReverseBits/190-ReverseBits.java
@@ -1,10 +1,10 @@
-// Last updated: 2/16/2026, 10:05:44 AM
+// Last updated: 2/16/2026, 10:07:33 AM
 1class Solution {
 2    public int reverseBits(int n) {
 3        int ans = 0;
 4        int pow = 31;
 5        while (n>0){
-6            if (n%2==1){
+6            if ((n&1)==1){
 7                ans += (int) Math.pow(2,pow);
 8            }
 9            pow--;
