2d42200ff327330ab3d0585abe24ec9f59c1ead9
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-26 11:19:07 +0530

    File updated at 2/26/2026, 11:19:05 AM

--- 2928-DistributeCandiesAmongChildrenI/2928-DistributeCandiesAmongChildrenI.cpp
+++ 2928-DistributeCandiesAmongChildrenI/2928-DistributeCandiesAmongChildrenI.cpp
@@ -1,17 +1,16 @@
-// Last updated: 2/26/2026, 11:17:04 AM
+// Last updated: 2/26/2026, 11:19:05 AM
 1class Solution {
 2public:
 3    int distributeCandies(int n, int limit) {
 4        int cnt = 0;
 5        for (int i=0 ; i<=limit ; i++){
 6            for (int j=0 ; j<=limit ; j++){
-7                for (int k=0 ; k<=limit ; k++){
-8                    if (i+j+k==n){
-9                        cnt++;
-10                    }
-11                }
-12            }
-13        }
-14        return cnt;
-15    }
-16};
+7                int left = n-i-j;
+8                if (left<=limit && left>=0){
+9                    cnt++;
+10                }
+11            }
+12        }
+13        return cnt;
+14    }
+15};
