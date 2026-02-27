95e36a8421b8ce0613728d4dd76868eca2a2c9bd
Sunil Singh <sunil.singh.030250@gmail.com>
2026-02-27 16:41:25 +0530

    File updated at 2/27/2026, 4:41:23 PM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,29 +1,27 @@
-// Last updated: 2/27/2026, 4:40:14 PM
+// Last updated: 2/27/2026, 4:41:23 PM
 1class Solution {
 2public:
 3    bool helper(vector<int>& arr, int idx, vector<bool>& visited){
-4        if (arr[idx]==0){
-5            return true;
-6        }
-7        if (visited[idx]){
-8            return false;
-9        }
-10        bool goLeft = false;
-11        if (idx-arr[idx]>=0){
-12            visited[idx] = true;
-13            goLeft = helper(arr,idx-arr[idx],visited);
-14            visited[idx] = false;
-15        }
-16        bool goRight = false;
-17        if (idx+arr[idx]<arr.size()){
-18            visited[idx] = true;
-19            goRight = helper(arr,idx+arr[idx],visited);
-20            visited[idx] = false;
-21        }
-22        return goLeft || goRight;
-23    }
-24    bool canReach(vector<int>& arr, int start) {
-25        vector<bool> visited(arr.size(),false);
-26        return helper(arr,start,visited);
-27    }
-28};
+4
+5        if (idx < 0 || idx >= arr.size())
+6            return false;
+7
+8        if (visited[idx])
+9            return false;
+10
+11        if (arr[idx] == 0)
+12            return true;
+13
+14        visited[idx] = true;
+15
+16        bool goLeft = helper(arr, idx - arr[idx], visited);
+17        bool goRight = helper(arr, idx + arr[idx], visited);
+18
+19        return goLeft || goRight;
+20    }
+21
+22    bool canReach(vector<int>& arr, int start) {
+23        vector<bool> visited(arr.size(), false);
+24        return helper(arr, start, visited);
+25    }
+26};
