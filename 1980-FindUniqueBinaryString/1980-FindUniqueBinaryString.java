228b0d3cb2ee3c72a61aea11144a7c7d2e0837da
Sunil Singh <sunil.singh.030250@gmail.com>
2026-03-08 07:49:51 +0530

    File updated at 3/8/2026, 7:49:50 AM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.java
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.java
@@ -1,34 +1,17 @@
-// Last updated: 3/8/2026, 7:48:55 AM
+// Last updated: 3/8/2026, 7:49:50 AM
 1class Solution {
-2    static boolean flag = false;
-3    static String ans = ""; 
-4    static List<String> ls = new ArrayList<>();
-5    public String findDifferentBinaryString(String[] nums) {
-6        ls = new ArrayList<>();
-7        for (String s : nums){
-8            ls.add(s);
-9        }
-10        int n = nums[0].length();
-11        lets(n,"");
-12        return ans;
-13    }
-14    public static void lets(int n , String str){
-15        if (n==0){
-16            boolean avl = true;
-17            for (String s : ls){
-18                if (s.equals(str)){
-19                    avl = false;
-20                    ls.remove(String.valueOf(s));
-21                        break;
-22                }
-23            }
-24            if (avl){
-25                flag = true;
-26                ans = str;
-27            }
-28            return;
-29        }
-30        lets(n-1,str+'0');
-31        lets(n-1,str+'1');
-32    }
-33}
+2    public String findDifferentBinaryString(String[] nums) {
+3        StringBuilder ans = new StringBuilder();
+4        for (int i=0;i<nums.length;i++){
+5            String s = nums[i];
+6            char ch = s.charAt(i);
+7            if (ch=='1'){
+8                ans.append('0');
+9            }
+10            else{
+11                ans.append('1');
+12            }
+13        }
+14        return ans.toString();
+15    }
+16}
