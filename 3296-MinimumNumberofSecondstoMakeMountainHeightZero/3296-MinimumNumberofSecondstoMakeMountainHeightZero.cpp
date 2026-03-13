8176fb34b716f57bf0e0be72052945339e47f501
Sunil Singh <sunil.singh.030250@gmail.com>
2026-03-13 14:31:41 +0530

    File updated at 3/13/2026, 2:31:40 PM

--- 3296-MinimumNumberofSecondstoMakeMountainHeightZero/3296-MinimumNumberofSecondstoMakeMountainHeightZero.cpp
+++ 3296-MinimumNumberofSecondstoMakeMountainHeightZero/3296-MinimumNumberofSecondstoMakeMountainHeightZero.cpp
@@ -1,57 +1,55 @@
-// Last updated: 3/13/2026, 2:31:12 PM
+// Last updated: 3/13/2026, 2:31:40 PM
 1class Solution {
 2public:
 3    int calH(int w, long long k){
-4        //w*(n*(n+1)/2)<=k
-5        
-6        int st = 1;
-7        int end = 1000000;
-8        int hei = 0;
-9        while (st<=end){
-10            int mid = (st+end)/2;
-11            long long m = mid;
-12            long long val = w*((m*(m+1))/2);
-13            if (val<=k){
-14                hei = mid;
-15                st = mid+1;
-16            }
-17            else{
-18                end = mid-1;
-19            }
-20        }
-21        return hei;
-22
-23
-24    }
-25    bool ispossible(vector<int>& wT, int h, long long k){
-26        //cout << k ;
-27        for (int w : wT){
-28            h -=  calH(w,k);
-29            if (h<1){
-30                //cout << "true" << endl;
-31                return true;
-32            }
-33        }
-34        //cout << "false" << endl;
-35        return false;
-36    }
-37
-38    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-39        
-40        long long minT = 1;
-41        long long maxT = LLONG_MAX-1;
-42        long long ans = minT;
-43        //cout << calH(5, (minT+maxT)/((long long) 2)) << endl;
-44        while (minT<=maxT){
-45            long long mid = (minT+maxT)/((long long) 2);
-46            if (ispossible(workerTimes,mountainHeight,mid)){
-47                ans = mid;
-48                maxT = mid-1;
-49            }
-50            else{
-51                minT = mid+1;
-52            }
-53        }
-54        return ans;
-55    }
-56};
+4        int st = 1;
+5        int end = 1000000;
+6        int hei = 0;
+7        while (st<=end){
+8            int mid = (st+end)/2;
+9            long long m = mid;
+10            long long val = w*((m*(m+1))/2);
+11            if (val<=k){
+12                hei = mid;
+13                st = mid+1;
+14            }
+15            else{
+16                end = mid-1;
+17            }
+18        }
+19        return hei;
+20
+21
+22    }
+23    bool ispossible(vector<int>& wT, int h, long long k){
+24        
+25        for (int w : wT){
+26            h -=  calH(w,k);
+27            if (h<1){
+28                
+29                return true;
+30            }
+31        }
+32        
+33        return false;
+34    }
+35
+36    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+37        
+38        long long minT = 1;
+39        long long maxT = LLONG_MAX-1;
+40        long long ans = minT;
+41        
+42        while (minT<=maxT){
+43            long long mid = (minT+maxT)/((long long) 2);
+44            if (ispossible(workerTimes,mountainHeight,mid)){
+45                ans = mid;
+46                maxT = mid-1;
+47            }
+48            else{
+49                minT = mid+1;
+50            }
+51        }
+52        return ans;
+53    }
+54};
