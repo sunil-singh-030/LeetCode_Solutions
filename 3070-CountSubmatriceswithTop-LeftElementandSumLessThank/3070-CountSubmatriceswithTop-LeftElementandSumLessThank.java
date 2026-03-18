1c824e66b5c103723ac9b2efb2200c0bd63f4f9a
Sunil Singh <sunil.singh.030250@gmail.com>
2026-03-18 21:15:42 +0530

    File updated at 3/18/2026, 9:15:41 PM

--- 3070-CountSubmatriceswithTop-LeftElementandSumLessThank/3070-CountSubmatriceswithTop-LeftElementandSumLessThank.java
+++ 3070-CountSubmatriceswithTop-LeftElementandSumLessThank/3070-CountSubmatriceswithTop-LeftElementandSumLessThank.java
@@ -1,33 +1,32 @@
-// Last updated: 3/18/2026, 9:13:52 PM
+// Last updated: 3/18/2026, 9:15:41 PM
 1class Solution {
 2    public int countSubmatrices(int[][] grid, int k) {
 3        int m = grid.length;
 4        int n = grid[0].length;
 5
-6        int[][] newGrid = new int[m][n];
-7
-8        for (int i=0 ; i<n ; i++){
-9            int sum = 0;
-10            for (int j=0 ; j<m ; j++){
-11                sum += grid[j][i];
-12                newGrid[j][i] = sum;
-13            }
-14        }
-15
-16        int tcnt = 0;
-17        for (int i=0 ; i<m ; i++){
-18            int tsum = 0;
-19            for (int j=0 ; j<n ; j++){
-20                tsum += newGrid[i][j];
-21                if (tsum<=k){
-22                    tcnt++;
-23                }
-24                else{
-25                    break;
-26                }
-27            }
-28        }
-29
-30        return tcnt;
-31    }
-32}
+6
+7        for (int i=0 ; i<n ; i++){
+8            int sum = 0;
+9            for (int j=0 ; j<m ; j++){
+10                sum += grid[j][i];
+11                grid[j][i] = sum;
+12            }
+13        }
+14
+15        int tcnt = 0;
+16        for (int i=0 ; i<m ; i++){
+17            int tsum = 0;
+18            for (int j=0 ; j<n ; j++){
+19                tsum += grid[i][j];
+20                if (tsum<=k){
+21                    tcnt++;
+22                }
+23                else{
+24                    break;
+25                }
+26            }
+27        }
+28
+29        return tcnt;
+30    }
+31}
