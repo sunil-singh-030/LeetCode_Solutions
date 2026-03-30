0df4d56a387999ef8a455f6889f868ff17a32fc9
Sunil Singh <sunil.singh.030250@gmail.com>
2026-03-30 06:23:23 +0530

    File updated at 3/30/2026, 6:23:23 AM

--- 2840-CheckifStringsCanbeMadeEqualWithOperationsII/2840-CheckifStringsCanbeMadeEqualWithOperationsII.java
+++ 2840-CheckifStringsCanbeMadeEqualWithOperationsII/2840-CheckifStringsCanbeMadeEqualWithOperationsII.java
@@ -1,33 +1,34 @@
-// Last updated: 3/30/2026, 6:20:08 AM
+// Last updated: 3/30/2026, 6:23:23 AM
 1class Solution {
 2    public boolean checkStrings(String s1, String s2) {
 3        int n = s2.length();
 4
-5        List<Character> l1 = new ArrayList<>();
-6        List<Character> l2 = new ArrayList<>();
+5        int[] freq = new int[26];
+6        
 7        for (int i=0 ; i<n ; i+=2){
-8            l1.add(s1.charAt(i));
-9            l2.add(s2.charAt(i));
+8            char ch = s1.charAt(i);
+9            freq[ch-'a']++;
 10        }
-11        Collections.sort(l1);
-12        Collections.sort(l2);
-13        for (int i=0 ; i<l1.size() ; i++){
-14            if (l1.get(i)!=l2.get(i)) return false;
-15        }
-16
-17        l1.clear();
-18        l2.clear();
-19
+11        for (int i=0 ; i<n ; i+=2){
+12            char ch = s2.charAt(i);
+13            freq[ch-'a']--;
+14        }
+15        for (int i=0 ; i<26 ; i++){
+16            if (freq[i]!=0) return false;
+17        }
+18        freq = new int[26];
+19        
 20        for (int i=1 ; i<n ; i+=2){
-21            l1.add(s1.charAt(i));
-22            l2.add(s2.charAt(i));
+21            char ch = s1.charAt(i);
+22            freq[ch-'a']++;
 23        }
-24        Collections.sort(l1);
-25        Collections.sort(l2);
-26
-27        for (int i=0 ; i<l1.size() ; i++){
-28            if (l1.get(i)!=l2.get(i)) return false;
-29        }
-30        return true;
-31    }
-32}
+24        for (int i=1 ; i<n ; i+=2){
+25            char ch = s2.charAt(i);
+26            freq[ch-'a']--;
+27        }
+28        for (int i=0 ; i<26 ; i++){
+29            if (freq[i]!=0) return false;
+30        }
+31        return true;
+32    }
+33}
