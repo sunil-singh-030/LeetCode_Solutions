aa87a626db282a151b6236909ba3ef8f5ec86b17
Sunil Singh <sunil.singh.030250@gmail.com>
2026-03-31 11:25:29 +0530

    File updated at 3/31/2026, 11:25:29 AM

--- 97-InterleavingString/97-InterleavingString.java
+++ 97-InterleavingString/97-InterleavingString.java
@@ -1,34 +1,32 @@
-// Last updated: 3/31/2026, 11:20:56 AM
+// Last updated: 3/31/2026, 11:25:29 AM
 1class Solution {
 2    public boolean isInterleave(String s1, String s2, String s3) {
 3        int n1 = s1.length();
 4        int n2 = s2.length();
 5        int n3 = s3.length();
-6        int[][][] dp = new int[n1+1][n2+1][n3+1];
-7        for (int[][] a : dp){
-8            for (int[] b : a){
-9                Arrays.fill(b,-1);
-10            }
-11        }
-12        return fn(s1,s2,s3,0,0,0,dp);
-13    }
-14    public boolean fn(String s1, String s2, String s3, int i, int j, int k, int[][][] dp){
-15        if (k==s3.length()){
-16            if (i==s1.length() && j==s2.length()){
-17                return true;
-18            }
-19            return false;
-20        }
-21        
-22        if (dp[i][j][k]!=-1) return dp[i][j][k]==1;
-23        boolean res = false;
-24        if (i<s1.length() && s1.charAt(i)==s3.charAt(k)){
-25            res = res || fn(s1,s2,s3,i+1,j,k+1,dp);
-26        }
-27        if (j<s2.length() && s2.charAt(j)==s3.charAt(k)){
-28            res = res || fn(s1,s2,s3,i,j+1,k+1,dp);
-29        }
-30        dp[i][j][k] = res ? 1 : 0;
-31        return res;
-32    }
-33}
+6        int[][] dp = new int[n1+1][n2+1];
+7        for (int[] a : dp){
+8            Arrays.fill(a,-1);
+9        }
+10        return fn(s1,s2,s3,0,0,dp);
+11    }
+12    public boolean fn(String s1, String s2, String s3, int i, int j, int[][] dp){
+13        if (i+j==s3.length()){
+14            if (i==s1.length() && j==s2.length()){
+15                return true;
+16            }
+17            return false;
+18        }
+19            
+20        if (dp[i][j]!=-1) return dp[i][j]==1;
+21        boolean res = false;
+22        if (i<s1.length() && s1.charAt(i)==s3.charAt(i+j)){
+23            res = res || fn(s1,s2,s3,i+1,j,dp);
+24        }
+25        if (j<s2.length() && s2.charAt(j)==s3.charAt(i+j)){
+26            res = res || fn(s1,s2,s3,i,j+1,dp);
+27        }
+28        dp[i][j] = res ? 1 : 0;
+29        return res;
+30    }
+31}
