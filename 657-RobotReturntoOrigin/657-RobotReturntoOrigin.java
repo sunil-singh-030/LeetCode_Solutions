0aed8915d919e6757d2b82388317ed75e6d8d7b1
Sunil Singh <sunil.singh.030250@gmail.com>
2026-04-05 10:46:48 +0530

    File updated at 4/5/2026, 10:46:47 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
@@ -1,22 +1,22 @@
-// Last updated: 4/5/2026, 10:45:22 AM
-1class Solution {
-2    public boolean judgeCircle(String moves) {
-3        int updown = 0;
-4        int leftright = 0;
-5        for (char ch : moves.toCharArray()){
-6            if (ch=='L'){
-7                leftright++;
-8            }
-9            else if (ch=='R'){
-10                leftright--;
-11            }
-12            else if (ch=='U'){
-13                updown++;
-14            }
-15            else{
-16                updown--;
-17            }
-18        }
-19        return (updown==0 && leftright==0) ;
-20    }
-21}
+// Last updated: 4/5/2026, 10:46:47 AM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int vert = 0;
+        int horizantal = 0;
+        for (int i = 0; i < moves.length(); i++){
+            if (moves.charAt(i) == 'D'){
+                vert = vert-1;
+            }
+            if (moves.charAt(i) == 'U'){
+                vert = vert+1;
+            }
+            if (moves.charAt(i) == 'R'){
+                horizantal = horizantal-1;
+            }
+            if (moves.charAt(i) == 'L'){
+                horizantal = horizantal+1;
+            }
+        }
+        return horizantal == 0 && vert == 0;
+    }
+}
