adc4077f4bb61e906a034f6b3ad3472e3c9e54d2
Sunil Singh <sunil.singh.030250@gmail.com>
2026-04-06 15:43:19 +0530

    File updated at 4/6/2026, 3:43:18 PM

--- 3891-MinimumIncreasetoMaximizeSpecialIndices/3891-MinimumIncreasetoMaximizeSpecialIndices.java
+++ 3891-MinimumIncreasetoMaximizeSpecialIndices/3891-MinimumIncreasetoMaximizeSpecialIndices.java
@@ -1,45 +1,46 @@
-// Last updated: 4/6/2026, 3:43:01 PM
+// Last updated: 4/6/2026, 3:43:18 PM
 1class Solution {
 2    public long minIncrease(int[] nums) {
 3        int n = nums.length;
-4        long[][] dp = new long[n+1][2];
-5        for (long[] a : dp){
-6            Arrays.fill(a,-1);
-7        }
-8        if (n%2==1){
-9            long cntOp = 0;
-10            for (int i=1 ; i<n-1 ; i+=2){
-11                if (nums[i]<=Math.max(nums[i-1],nums[i+1])){
-12                    cntOp += Math.max(nums[i-1],nums[i+1])-nums[i]+1;
-13                }
-14            }
-15            return cntOp;
-16        }
-17        return jaiBabaKi(nums,1,1,dp);
-18        
-19
-20    }
-21
-22    public long jaiBabaKi(int[] nums, int idx, int canI, long[][] dp){
-23        if (idx>=nums.length-1){
-24            return 0;
-25        }
-26        if (dp[idx][canI]!=-1) return dp[idx][canI];
-27
-28        long res = Long.MAX_VALUE/2;
-29        if (canI==1){
-30            res = jaiBabaKi(nums,idx+1,0,dp);
-31        }
-32        
-33        int reqHeight = 0;
-34        int maxHeight = Math.max(nums[idx-1],nums[idx+1]);
-35        if (nums[idx]<=maxHeight){
-36            reqHeight = maxHeight-nums[idx]+1;
-37        }
-38        long take = jaiBabaKi(nums,idx+2,canI,dp);
-39        if (take!=Long.MAX_VALUE/2){
-40            res = Math.min(res,reqHeight + take );
-41        }
-42        return dp[idx][canI] = res;
-43    }
-44}
+4        
+5        if (n%2==1){
+6            long cntOp = 0;
+7            for (int i=1 ; i<n-1 ; i+=2){
+8                if (nums[i]<=Math.max(nums[i-1],nums[i+1])){
+9                    cntOp += Math.max(nums[i-1],nums[i+1])-nums[i]+1;
+10                }
+11            }
+12            return cntOp;
+13        }
+14        long[][] dp = new long[n+1][2];
+15        for (long[] a : dp){
+16            Arrays.fill(a,-1);
+17        }
+18        return jaiBabaKi(nums,1,1,dp);
+19        
+20
+21    }
+22
+23    public long jaiBabaKi(int[] nums, int idx, int canI, long[][] dp){
+24        if (idx>=nums.length-1){
+25            return 0;
+26        }
+27        if (dp[idx][canI]!=-1) return dp[idx][canI];
+28
+29        long res = Long.MAX_VALUE/2;
+30        if (canI==1){
+31            res = jaiBabaKi(nums,idx+1,0,dp);
+32        }
+33        
+34        int reqHeight = 0;
+35        int maxHeight = Math.max(nums[idx-1],nums[idx+1]);
+36        if (nums[idx]<=maxHeight){
+37            reqHeight = maxHeight-nums[idx]+1;
+38        }
+39        long take = jaiBabaKi(nums,idx+2,canI,dp);
+40        if (take!=Long.MAX_VALUE/2){
+41            res = Math.min(res,reqHeight + take );
+42        }
+43        return dp[idx][canI] = res;
+44    }
+45}
