58dc6188320729535d9ad2dea1dec5d057b65446
Sunil Singh <sunil.singh.030250@gmail.com>
2026-04-18 08:33:37 +0530

    File updated at 4/18/2026, 8:33:38 AM

--- 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.java
+++ 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.java
@@ -1,11 +1,11 @@
-// Last updated: 4/18/2026, 7:58:36 AM
+// Last updated: 4/18/2026, 8:33:38 AM
 1class Solution {
 2    public int mirrorDistance(int n) {
 3        String s = String.valueOf(n);
-4        String sr = "";
+4        StringBuilder sr = new StringBuilder();
 5        for (int i=s.length()-1 ; i>=0 ; i--){
-6            sr += s.charAt(i);
+6            sr.append(s.charAt(i));
 7        }
-8        return Math.abs(n-Integer.valueOf(sr));
+8        return Math.abs(n-Integer.valueOf(sr.toString()));
 9    }
 10}
