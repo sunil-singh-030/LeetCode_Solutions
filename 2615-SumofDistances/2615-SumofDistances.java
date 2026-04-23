f85005df776d73fb9137d10f67c154f64768af25
Sunil Singh <sunil.singh.030250@gmail.com>
2026-04-23 06:21:16 +0530

    File updated at 4/23/2026, 6:21:15 AM

--- 2615-SumofDistances/2615-SumofDistances.java
+++ 2615-SumofDistances/2615-SumofDistances.java
@@ -1,4 +1,4 @@
-// Last updated: 4/23/2026, 6:20:55 AM
+// Last updated: 4/23/2026, 6:21:15 AM
 1class Solution {
 2    // [0,1,3,5,6,8]  [0,1,4,9,15,23]
 3    // [0,0,10,4,2,0]
@@ -12,7 +12,7 @@
 11            }
 12            map.get(num).add(i);
 13        }
-14        //HashMap<Integer,long[][]> map2 = new HashMap<>();
+14        
 15        long[] arr = new long[n];
 16        for (int key : map.keySet()){
 17            List<Integer> ls = map.get(key);
@@ -30,28 +30,7 @@
 29            }
 30        }
 31        
-32        // for (int i=0 ; i<n ; i++){
-33        //     List<Integer> ls = map.get(nums[i]);
-34        //     long[][] temp = map2.get(nums[i]);
-35        //     int len = ls.size();
-36        //     int idx = -1;
-37        //     int st = 0;
-38        //     int end = len-1;
-39        //     while (st<=end){
-40        //         int mid = (st+end)/2;
-41        //         if (ls.get(mid)==i){
-42        //             idx = mid;
-43        //             break;
-44        //         }
-45        //         else if (ls.get(mid)>i){
-46        //             end = mid-1;
-47        //         }
-48        //         else{
-49        //             st = mid+1;
-50        //         }
-51        //     }
-52        //     arr[i] = temp[0][idx] + temp[1][idx];
-53        // }
-54        return arr;
-55    }
-56}
+32        
+33        return arr;
+34    }
+35}
