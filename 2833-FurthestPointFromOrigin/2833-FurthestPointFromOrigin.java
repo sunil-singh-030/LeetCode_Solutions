eeda9a93c3b197c4d23b286960618f68978ca2e5
Sunil Singh <sunil.singh.030250@gmail.com>
2026-04-24 06:37:34 +0530

    File updated at 4/24/2026, 6:37:33 AM

--- 2833-FurthestPointFromOrigin/2833-FurthestPointFromOrigin.java
+++ 2833-FurthestPointFromOrigin/2833-FurthestPointFromOrigin.java
@@ -1,20 +1,19 @@
-// Last updated: 4/24/2026, 6:36:43 AM
+// Last updated: 4/24/2026, 6:37:33 AM
 1class Solution {
 2    public int furthestDistanceFromOrigin(String moves) {
-3        int l = 0;
-4        int r = 0;
-5        int sp = 0;
-6        for (char ch : moves.toCharArray()){
-7            if (ch=='L'){
-8                l++;
-9            }
-10            else if (ch=='R'){
-11                r++;
-12            }
-13            else{
-14                sp++;
-15            }
-16        }
-17        return Math.abs(l-r)+sp;
-18    }
-19}
+3        int dis = 0;
+4        int sp = 0;
+5        for (char ch : moves.toCharArray()){
+6            if (ch=='L'){
+7                dis++;
+8            }
+9            else if (ch=='R'){
+10                dis--;
+11            }
+12            else{
+13                sp++;
+14            }
+15        }
+16        return Math.abs(dis)+sp;
+17    }
+18}
