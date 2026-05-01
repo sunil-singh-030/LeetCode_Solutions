4bb1875acdef910e9325fc6c063336fd3da847de
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-01 08:52:10 +0530

    File updated at 5/1/2026, 8:52:10 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,23 +1,25 @@
-// Last updated: 5/1/2026, 8:50:11 AM
+// Last updated: 5/1/2026, 8:52:10 AM
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int m = text1.length();
 4        int n = text2.length();
-5        int[][] dp = new int[m+1][n+1];
-6        
-7        for (int i=1 ; i<=m ; i++){
-8            for (int j=1 ; j<=n ; j++){
-9                char ch1 = text1.charAt(i-1);
-10                char ch2 = text2.charAt(j-1);
-11                if (ch1==ch2){
-12                    dp[i][j] = 1 + dp[i-1][j-1];
-13                }
-14                else{
-15                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-16                }
-17            }
-18        }
-19        return dp[m][n];
-20    }
-21    
-22}
+5        int[] prev = new int[n+1];
+6        int[] curr = new int[n+1];
+7        
+8        for (int i=1 ; i<=m ; i++){
+9            for (int j=1 ; j<=n ; j++){
+10                char ch1 = text1.charAt(i-1);
+11                char ch2 = text2.charAt(j-1);
+12                if (ch1==ch2){
+13                    curr[j] = 1 + prev[j-1];
+14                }
+15                else{
+16                    curr[j] = Math.max(prev[j],curr[j-1]);
+17                }
+18            }
+19            prev = curr.clone();
+20        }
+21        return curr[n];
+22    }
+23    
+24}
