f5e90fa2af57941ea544d28a7405cee4b1e29a60
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-01 08:50:11 +0530

    File updated at 5/1/2026, 8:50:11 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,29 +1,23 @@
-// Last updated: 5/1/2026, 8:44:32 AM
+// Last updated: 5/1/2026, 8:50:11 AM
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int m = text1.length();
 4        int n = text2.length();
-5        int[][] dp = new int[m][n];
-6        for (int[] a : dp){
-7            Arrays.fill(a,-1);
-8        }
-9        return helper(text1,m-1,text2,n-1,dp);
-10    }
-11    public int helper(String s1, int idx1, String s2, int idx2, int[][] dp){
-12        if (idx1<0 || idx2<0){
-13            return 0;
-14        }
-15        if (dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-16        char ch1 = s1.charAt(idx1);
-17        char ch2 = s2.charAt(idx2);
-18
-19        if (ch1==ch2){
-20            return dp[idx1][idx2] = 1 + helper(s1,idx1-1,s2,idx2-1,dp);
-21        }
-22        else{
-23            int skipCh1 = 0 + helper(s1,idx1-1,s2,idx2,dp);
-24            int skipCh2 = 0 + helper(s1,idx1,s2,idx2-1,dp);
-25            return dp[idx1][idx2] = Math.max(skipCh1,skipCh2);
-26        }
-27    }
-28}
+5        int[][] dp = new int[m+1][n+1];
+6        
+7        for (int i=1 ; i<=m ; i++){
+8            for (int j=1 ; j<=n ; j++){
+9                char ch1 = text1.charAt(i-1);
+10                char ch2 = text2.charAt(j-1);
+11                if (ch1==ch2){
+12                    dp[i][j] = 1 + dp[i-1][j-1];
+13                }
+14                else{
+15                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+16                }
+17            }
+18        }
+19        return dp[m][n];
+20    }
+21    
+22}
