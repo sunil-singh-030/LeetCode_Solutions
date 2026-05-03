36b6da6ed67ecfeb1137d8c86bc381ead46f3f3a
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-03 16:16:54 +0530

    File updated at 5/3/2026, 4:16:52 PM

--- 3917-CountIndicesWithOppositeParity/3917-CountIndicesWithOppositeParity.java
+++ 3917-CountIndicesWithOppositeParity/3917-CountIndicesWithOppositeParity.java
@@ -1,33 +1,46 @@
-// Last updated: 5/3/2026, 3:53:50 PM
+// Last updated: 5/3/2026, 4:16:52 PM
 1class Solution {
-2    public int sumOfPrimesInRange(int n) {
-3        boolean[] isprime = new boolean[1001];
-4        Arrays.fill(isprime,true);
-5        isprime[0] = false;
-6        isprime[1] = false;
-7        for (int i=2 ; i*i<1001 ; i++){
-8            if (isprime[i]){
-9                for (int j=i+i ; j<1001 ; j+=i){
-10                    isprime[j] = false;
-11                }
-12            }
-13        }
-14        int[] sumprime = new int[1001];
-15        for (int i=1 ; i<1001 ; i++){
-16            sumprime[i] = sumprime[i-1];
-17            if (isprime[i]){
-18                sumprime[i] += i;
-19            }
-20        }
-21        int newNum = 0;
-22        int temp = n;
-23        while (temp>0){
-24            newNum *= 10;
-25            newNum += temp%10;
-26            temp /= 10;
+2    public int[] minCost(int[] nums, int[][] queries) {
+3        int n = nums.length;
+4
+5        int[] right = new int[n+1];
+6        right[1] = 1;
+7        for (int i=1 ; i<n-1 ; i++){
+8            right[i+1] = right[i];
+9            if (Math.abs(nums[i]-nums[i+1])<Math.abs(nums[i]-nums[i-1])){
+10                right[i+1]++;
+11            }
+12            else{
+13                right[i+1] += Math.abs(nums[i]-nums[i+1]);
+14            }
+15        }
+16
+17        int[] left = new int[n+1];
+18        left[n-1] = 1;
+19        for (int i=n-2 ; i>0 ; i--){
+20            left[i] = left[i+1];
+21            if (Math.abs(nums[i]-nums[i-1])<=Math.abs(nums[i]-nums[i+1])){
+22                left[i]++;
+23            }
+24            else{
+25                left[i] += Math.abs(nums[i]-nums[i-1]);
+26            }
 27        }
-28        int min = Math.min(n,newNum);
-29        int max = Math.max(n,newNum);
-30        return sumprime[max]-sumprime[min-1];
-31    }
-32}
+28
+29        int[] ans = new int[queries.length];
+30        for (int i=0 ; i<queries.length ; i++){
+31            int st = queries[i][0];
+32            int end = queries[i][1];
+33            if (st<=end){
+34                ans[i] = right[end]-right[st];
+35            }
+36            else{
+37                ans[i] = left[end+1]-left[st+1];
+38            }
+39        }
+40        // System.out.println(Arrays.toString(right));
+41        // System.out.println(Arrays.toString(left));
+42        return ans;
+43
+44    }
+45}
