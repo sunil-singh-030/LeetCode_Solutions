709bdec76167c7b1181e912215bac7da277d0ea2
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-04 06:43:20 +0530

    File updated at 5/4/2026, 6:43:19 AM

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,33 +1,33 @@
-// Last updated: 9/29/2025, 10:50:33 AM
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int minr = 0;
-        int minc = 0;
-        int maxr = n-1;
-        int maxc = n-1;
-        int move = n/2;
-        int movecnt = n-1;
-        while (move-->0){
-            for (int i=0 ; i<movecnt ; i++){
-                int temp1 = matrix[minr][minc+i];
-                int temp2 = matrix[minr+i][maxc];
-                int temp3 = matrix[maxr][maxc-i];
-                int temp4 = matrix[maxr-i][minc];
-                matrix[minr][minc+i] = temp4;
-                matrix[minr+i][maxc] = temp1;
-                matrix[maxr][maxc-i] = temp2;
-                matrix[maxr-i][minc] = temp3;
-            }
-            movecnt -= 2;
-            minr++;
-            minc++;
-            maxr--;
-            maxc--;
-            // for (int[] a : matrix){
-            //     System.out.println(Arrays.toString(a));
-            // }
-        }
-        
-    }
-}
+// Last updated: 5/4/2026, 6:43:19 AM
+1class Solution {
+2    public void rotate(int[][] matrix) {
+3        int n = matrix.length;
+4        int minr = 0;
+5        int minc = 0;
+6        int maxr = n-1;
+7        int maxc = n-1;
+8        int move = n/2;
+9        int movecnt = n-1;
+10        while (move-->0){
+11            for (int i=0 ; i<movecnt ; i++){
+12                int temp1 = matrix[minr][minc+i];
+13                int temp2 = matrix[minr+i][maxc];
+14                int temp3 = matrix[maxr][maxc-i];
+15                int temp4 = matrix[maxr-i][minc];
+16                matrix[minr][minc+i] = temp4;
+17                matrix[minr+i][maxc] = temp1;
+18                matrix[maxr][maxc-i] = temp2;
+19                matrix[maxr-i][minc] = temp3;
+20            }
+21            movecnt -= 2;
+22            minr++;
+23            minc++;
+24            maxr--;
+25            maxc--;
+26            // for (int[] a : matrix){
+27            //     System.out.println(Arrays.toString(a));
+28            // }
+29        }
+30        
+31    }
+32}
