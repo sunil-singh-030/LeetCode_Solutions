ba0442d3b2cf87a5d476084c5a53c5e97692c333
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-16 11:07:36 +0530

    File updated at 5/16/2026, 11:07:34 AM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,29 +1,18 @@
-// Last updated: 10/5/2025, 7:23:22 AM
-class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if (n==1){
-            return nums[0];
-        }
-        int prev = nums[1];
-        int prev2 = nums[0];
-        int prev3 = -1;
-        for (int i=2 ; i<n ; i++){
-             if (i>2){
-                int temp1 = prev;
-                int temp2 = prev2;
-                prev = nums[i] + Math.max(prev2,prev3);
-                prev2 = temp1;
-                prev3 = temp2;
-             }
-             else{
-                int temp1 = prev;
-                int temp2 = prev2;
-                prev = nums[i] + prev2;
-                prev2 = temp1;
-                prev3 = temp2;;
-             }
-        }
-        return Math.max(prev2,prev);
-    }
-}
+// Last updated: 5/16/2026, 11:07:34 AM
+1class Solution {
+2    public int rob(int[] nums) {
+3        int n = nums.length;
+4        int[] dp = new int[n+1];
+5        Arrays.fill(dp,-1);
+6        return helper(nums,0,dp);
+7    }
+8    public int helper(int[] nums, int idx, int[] dp){
+9        if (idx>=nums.length){
+10            return 0;
+11        }
+12        if (dp[idx]!=-1) return dp[idx];
+13        int skip = helper(nums,idx+1,dp);
+14        int rob = nums[idx] + helper(nums,idx+2,dp);
+15        return dp[idx] = Math.max(skip,rob);
+16    }
+17}
