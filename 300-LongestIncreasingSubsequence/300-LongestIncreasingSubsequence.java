c1a746ffb15c43efef31474d032a6423ed31ca9d
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-16 12:01:09 +0530

    File updated at 5/16/2026, 12:01:07 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,18 +1,37 @@
-// Last updated: 5/16/2026, 11:53:51 AM
+// Last updated: 5/16/2026, 12:01:07 PM
 1class Solution {
 2    public int lengthOfLIS(int[] nums) {
 3        int n = nums.length;
 4        int ans = 1;
-5        int[] dp = new int[n];
-6        Arrays.fill(dp,1);
-7        for (int i=0 ; i<n ; i++){
-8            for (int j=i-1 ; j>=0 ; j--){
-9                if (nums[i]>nums[j]){
-10                    dp[i] = Math.max(dp[i],1+dp[j]);
-11                }
-12            }
-13            ans = Math.max(ans,dp[i]);
-14        }
-15        return ans;
-16    }
-17}
+5        List<Integer> ls = new ArrayList<>();
+6        for (int i=0 ; i<n ; i++){
+7            if (ls.size()==0){
+8                ls.add(nums[i]);
+9            }
+10            else{
+11                if (nums[i]>ls.get(ls.size()-1)){
+12                    ls.add(nums[i]);
+13                }
+14                else{
+15                    int st = 0;
+16                    int end = ls.size()-1;
+17                    int idx = 0;
+18                    while (st<=end){
+19                        int mid = (st+end)/2;
+20                        if (ls.get(mid)>=nums[i]){
+21                            idx = mid;
+22                            end = mid-1;
+23                        }
+24                        else{
+25                            st = mid+1;
+26                        }
+27                    }
+28                    ls.set(idx,nums[i]);
+29                }
+30                
+31            }
+32            ans = Math.max(ans,ls.size());
+33        }
+34        return ans;
+35    }
+36}
