3582ef61636840f79495d47331ce5f88791e008b
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-17 07:07:38 +0530

    File updated at 5/17/2026, 7:07:35 AM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,15 +1,25 @@
-// Last updated: 8/1/2025, 7:12:08 AM
-class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int goal = n-1;
-        for (int i=n-2 ; i>=0 ; i--){
-            if (i+nums[i]>=goal){
-                goal = i;
-            }
-        }
-        return goal==0;
-    } 
-    
-    
-}
+// Last updated: 5/17/2026, 7:07:35 AM
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int n = nums.length;
+4        boolean[] canI = new boolean[n];
+5        canI[n-1] = true;
+6        for (int i=n-2 ; i>=0 ; i--){
+7            int jump = nums[i];
+8            if (i+jump>=n-1){
+9                canI[i] = true;
+10            }
+11            else{
+12                
+13                for (int j=i+1 ; j<=i+jump ; j++){
+14                    if (canI[j]){
+15                        canI[i] = true;
+16                        break;
+17                    }
+18                }
+19            }
+20            
+21        }
+22        return canI[0];
+23    }
+24}
