9d9dc385bdbb253fd31c256e2c1db9c437320e1d
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-17 07:16:11 +0530

    File updated at 5/17/2026, 7:16:08 AM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,24 +1,15 @@
-// Last updated: 5/17/2026, 7:11:50 AM
+// Last updated: 5/17/2026, 7:16:08 AM
 1class Solution {
 2    public boolean canJump(int[] nums) {
 3        int n = nums.length;
-4        int[] dp = new int[n];
-5        Arrays.fill(dp,-1);
-6        return helper(nums,0,dp);
-7    }
-8    public boolean helper(int[] nums, int idx, int[] dp){
-9        if (idx>=nums.length-1){
-10            return true;
-11        }
-12        if (dp[idx]!=-1) return dp[idx]==1;
-13        int jumps = nums[idx];
-14        for (int i=idx+1 ; i<=idx+jumps ; i++){
-15            if (helper(nums,i,dp)) {
-16                dp[idx] = 1;
-17                return true;
-18            }
-19        }
-20        dp[idx] = 0;
-21        return false;
-22    }
-23}
+4        int goal = n-1;
+5        for (int i=n-2 ; i>=0 ; i--){
+6            if (i+nums[i]>=goal){
+7                goal = i;
+8            }
+9        }
+10        return goal==0;
+11    } 
+12    
+13    
+14}
