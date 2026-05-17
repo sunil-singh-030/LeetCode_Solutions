d85db21adaf142d8354d5832c8a64d885d1cdc5b
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-17 20:29:07 +0530

    File updated at 5/17/2026, 8:29:04 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,22 +1,25 @@
-// Last updated: 8/1/2025, 7:10:54 AM
-import java.util.Arrays;
-
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-
-        Arrays.sort(nums);
-        int ans = 1, count = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) continue; // Skip duplicates
-            if (nums[i] == nums[i - 1] + 1) {
-                count++;
-            } else {
-                ans = Math.max(ans, count);
-                count = 1;
-            }
-        }
-        return Math.max(ans, count);
-    }
-}
+// Last updated: 5/17/2026, 8:29:04 PM
+1class Solution {
+2    public int longestConsecutive(int[] nums) {
+3        Arrays.sort(nums);
+4        int n = nums.length;
+5        if (n==0) return 0;
+6        int maxLen = 1;
+7        int currLen = 1;
+8        for (int i=1 ; i<n ; i++){
+9            if (nums[i]==nums[i-1]){
+10                continue;
+11            }
+12            if (nums[i]-nums[i-1]==1){
+13                currLen++;
+14            }
+15            else{
+16                maxLen = Math.max(maxLen,currLen);
+17                currLen = 1;
+18            }
+19        }
+20        maxLen = Math.max(maxLen,currLen);
+21        return maxLen;
+22        
+23    }
+24}
