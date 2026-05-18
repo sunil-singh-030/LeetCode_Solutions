c4425079a8d582e5bf73aae7957569b355252b68
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-18 22:15:40 +0530

    File updated at 5/18/2026, 10:15:36 PM

--- 3931-CheckAdjacentDigitDifferences/3931-CheckAdjacentDigitDifferences.java
+++ 3931-CheckAdjacentDigitDifferences/3931-CheckAdjacentDigitDifferences.java
@@ -1,13 +1,26 @@
-// Last updated: 5/18/2026, 10:05:50 PM
+// Last updated: 5/18/2026, 10:15:36 PM
 1class Solution {
-2    public boolean isAdjacentDiffAtMostTwo(String s) {
-3        int n = s.length();
-4        for (int i=1 ; i<n ; i++){
-5            char ch1 = s.charAt(i-1);
-6            char ch2 = s.charAt(i);
-7            if (Math.abs(ch1-ch2)>2) return false;
-8        }
-9        return  true;
-10
-11    }
-12}
+2    public int countKthRoots(int l, int r, int k) {
+3        System.out.println(helper(r+1,k));
+4        return helper(r+1,k)-helper(l,k);
+5    }
+6    public int helper(int num , int k){
+7        if (num==0) return 0;
+8        int st = 0;
+9        int end = num;
+10        int cnt = 0;
+11        while (st<=end){
+12            int mid = (st+end)/2;
+13            int val = (int) Math.pow(mid,k);
+14            if (val<num){
+15                cnt = mid;
+16                st = mid+1;
+17            }
+18            else{
+19                end = mid-1;
+20            }
+21
+22        }
+23        return cnt+1;
+24    }
+25}
