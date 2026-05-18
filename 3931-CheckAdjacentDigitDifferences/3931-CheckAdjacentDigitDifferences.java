110af161552dc1155bacf6f4478025d1ff084b07
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-18 22:15:50 +0530

    File updated at 5/18/2026, 10:15:45 PM

--- 3931-CheckAdjacentDigitDifferences/3931-CheckAdjacentDigitDifferences.java
+++ 3931-CheckAdjacentDigitDifferences/3931-CheckAdjacentDigitDifferences.java
@@ -1,26 +1,25 @@
-// Last updated: 5/18/2026, 10:15:39 PM
+// Last updated: 5/18/2026, 10:15:45 PM
 1class Solution {
 2    public int countKthRoots(int l, int r, int k) {
-3        System.out.println(helper(r+1,k));
-4        return helper(r+1,k)-helper(l,k);
-5    }
-6    public int helper(int num , int k){
-7        if (num==0) return 0;
-8        int st = 0;
-9        int end = num;
-10        int cnt = 0;
-11        while (st<=end){
-12            int mid = (st+end)/2;
-13            int val = (int) Math.pow(mid,k);
-14            if (val<num){
-15                cnt = mid;
-16                st = mid+1;
-17            }
-18            else{
-19                end = mid-1;
-20            }
-21
-22        }
-23        return cnt+1;
-24    }
-25}
+3        return helper(r+1,k)-helper(l,k);
+4    }
+5    public int helper(int num , int k){
+6        if (num==0) return 0;
+7        int st = 0;
+8        int end = num;
+9        int cnt = 0;
+10        while (st<=end){
+11            int mid = (st+end)/2;
+12            int val = (int) Math.pow(mid,k);
+13            if (val<num){
+14                cnt = mid;
+15                st = mid+1;
+16            }
+17            else{
+18                end = mid-1;
+19            }
+20
+21        }
+22        return cnt+1;
+23    }
+24}
