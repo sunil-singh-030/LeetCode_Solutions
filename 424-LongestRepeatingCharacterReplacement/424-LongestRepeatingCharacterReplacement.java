cd917e9de2a0f0bd6bad4089ef634e4b22dee94d
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-18 12:21:18 +0530

    File updated at 5/18/2026, 12:21:14 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -1,42 +1,37 @@
-// Last updated: 8/1/2025, 7:08:40 AM
-class Solution {
-    public int characterReplacement(String s, int k) {
-        int [] arr=new int[26];
-        for (int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'A']++;
-        }
-        int ans=1;
-        for (int i=0;i<26;i++){
-            if (arr[i]>0){
-                //System.out.println((char) (i+65));
-                int out=countsamelenstr(s,k,(char) (i+65));
-                ans=Math.max(ans,out);
-                //System.out.println(ans);
-            }
-        }
-        return ans;
-    }
-    public static int countsamelenstr(String s,int k,char ch){
-        int ans=1;
-        int si=0;
-        int ci=0;
-        int flip=0;
-        while (ci<s.length()){
-            // grow
-            if (s.charAt(ci)!=ch){
-                flip++;
-            }
-            // shrink
-            while (flip>k){
-                if (s.charAt(si)!=ch){
-                    flip--;
-                }
-                si++;
-            }
-            // ans update
-            ans=Math.max(ans,ci-si+1);
-            ci++;
-        }
-        return ans;
-    }
-}
+// Last updated: 5/18/2026, 12:21:14 PM
+1class Solution {
+2    public int characterReplacement(String s, int k) {
+3        // AABABBA K=1
+4        int n = s.length();
+5        HashMap<Character,Integer> map = new HashMap<>();
+6        int maxLen = 1;
+7        int ci = 0;
+8        int si = 0;
+9
+10        while (ci<n){
+11            char ch = s.charAt(ci);
+12            map.put(ch,map.getOrDefault(ch,0)+1);
+13            while (helper(map,ci-si+1)>k){
+14                char prev = s.charAt(si);
+15                int freq = map.get(prev);
+16                if (freq==1){
+17                    map.remove(prev);
+18                }
+19                else{
+20                    map.put(prev,freq-1);
+21                }
+22                si++;
+23            }
+24            maxLen = Math.max(maxLen,ci-si+1);
+25            ci++;
+26        }
+27        return maxLen;
+28    }
+29    public int helper(HashMap<Character,Integer> map, int cnt){
+30        int max = 0;
+31        for (char key : map.keySet()){
+32            max = Math.max(max,map.get(key));
+33        }
+34        return cnt-max;
+35    }
+36}
