27e0f2377e2396c239e8054abce95f83c52991ee
Sunil Singh <sunil.singh.030250@gmail.com>
2026-05-20 15:15:26 +0530

    File updated at 5/20/2026, 3:15:26 PM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,25 +1,25 @@
-// Last updated: 10/27/2025, 3:38:39 PM
-class Solution {
-    public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int prevmin = 1;
-        int prevmax = 1;
-        int ans = Integer.MIN_VALUE;
-        for (int i=0 ; i<n ; i++){
-            int num = nums[i];
-            int currMax = Math.max(num*prevmin,num*prevmax);
-            ans = Math.max(ans,currMax);
-            if (num>=0){
-                prevmax = Math.max(1,prevmax*num);
-                prevmin = Math.min(1,num*prevmin);
-            }
-            else{
-                int temp = prevmin;
-                prevmin = Math.min(1,prevmax*num);
-                prevmax = Math.max(1,temp*num);
-            }
-        }
-        return ans;
-    }
-    
-}
+// Last updated: 5/20/2026, 3:15:26 PM
+1class Solution {
+2    public int maxProduct(int[] nums) {
+3        int n = nums.length;
+4        int prevmin = 1;
+5        int prevmax = 1;
+6        int ans = Integer.MIN_VALUE;
+7        for (int i=0 ; i<n ; i++){
+8            int num = nums[i];
+9            int currMax = Math.max(num*prevmin,num*prevmax);
+10            ans = Math.max(ans,currMax);
+11            if (num>=0){
+12                prevmax = Math.max(1,prevmax*num);
+13                prevmin = Math.min(1,num*prevmin);
+14            }
+15            else{
+16                int temp = prevmin;
+17                prevmin = Math.min(1,prevmax*num);
+18                prevmax = Math.max(1,temp*num);
+19            }
+20        }
+21        return ans;
+22    }
+23    
+24}
