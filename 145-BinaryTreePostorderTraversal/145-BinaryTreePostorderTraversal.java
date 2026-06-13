91c48826422296f1a1ca2d4b1e80e79f6d0034bc
Sunil Singh <sunil.singh.030250@gmail.com>
2026-06-13 08:23:56 +0530

    File updated at 6/13/2026, 8:23:56 AM

--- 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 6/2/2026, 11:51:00 AM
+// Last updated: 6/13/2026, 8:23:56 AM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -18,19 +18,29 @@
 17    public List<Integer> postorderTraversal(TreeNode root) {
 18        List<Integer> ls = new ArrayList<>();
 19        if (root==null) return ls;
-20        Stack<TreeNode> st1 = new Stack<>();
-21        Stack<TreeNode> st2 = new Stack<>();
-22        st1.push(root);
-23        while (!st1.isEmpty()){
-24            TreeNode curr = st1.pop();
-25            st2.push(curr);
-26            if (curr.left!=null) st1.push(curr.left);
-27            if (curr.right!=null) st1.push(curr.right);
-28        }
-29        while (!st2.isEmpty()){
-30            ls.add(st2.pop().val);
-31        }
-32        return ls;
-33    }
-34    
-35}
+20
+21        Stack<TreeNode> st1 = new Stack<>();
+22        Stack<TreeNode> st2 = new Stack<>();
+23
+24        st1.push(root);
+25
+26        while (!st1.isEmpty()){
+27            TreeNode curr = st1.pop();
+28            st2.push(curr);
+29            
+30            if (curr.left!=null){
+31                st1.push(curr.left);
+32            }
+33            if (curr.right!=null){
+34                st1.push(curr.right);
+35            }
+36
+37        }
+38        while (!st2.isEmpty()){
+39            ls.add(st2.pop().val);
+40        }
+41        
+42        return ls;
+43    }
+44    
+45}
