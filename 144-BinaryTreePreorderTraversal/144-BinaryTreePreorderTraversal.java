ef59b37353d97a06f5d9995f9b9a2db7901c3584
Sunil Singh <sunil.singh.030250@gmail.com>
2026-06-13 06:50:17 +0530

    File updated at 6/13/2026, 6:50:18 AM

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 6/13/2026, 6:44:50 AM
+// Last updated: 6/13/2026, 6:50:18 AM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -17,17 +17,24 @@
 16class Solution {
 17    public List<Integer> preorderTraversal(TreeNode root) {
 18        List<Integer> ls = new ArrayList<>();
-19        helper(root,ls);
-20        return ls;
-21    }
-22
-23    public void helper(TreeNode curr, List<Integer> ls){
-24        if (curr==null){
-25            return;
-26        }
-27        ls.add(curr.val);
-28        helper(curr.left,ls);
-29        helper(curr.right,ls);
-30    }
-31    
-32}
+19        if (root==null){
+20            return ls;
+21        }
+22        Stack<TreeNode> st = new Stack<>();
+23        st.push(root);
+24
+25        while (!st.isEmpty()){
+26            TreeNode curr = st.pop();
+27            ls.add(curr.val);
+28
+29            if (curr.right!=null) st.push(curr.right);
+30            if (curr.left!=null) st.push(curr.left);
+31            
+32        }
+33
+34        return ls;
+35    }
+36
+37    
+38    
+39}
