2ed8eabdf2dec65a945cca3ea930c16468a79e16
Sunil Singh <sunil.singh.030250@gmail.com>
2026-07-17 07:20:12 +0530

    File updated at 7/17/2026, 7:20:12 AM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,33 +1,30 @@
-// Last updated: 8/1/2025, 7:12:53 AM
-class Solution {
-    public boolean isValid(String s) {
-        if (s.length()%2==1){
-            return false;
-        }
-        StringBuilder sb=new StringBuilder();
-        for (char ch:s.toCharArray()){
-            if (ch=='(' || ch=='[' || ch=='{'){
-                sb.append(ch);
-            }
-            else{
-                if (sb.length()==0){
-                    return false;
-                }
-                if (ch==')' && sb.charAt(sb.length()-1)=='('){
-                    sb.deleteCharAt(sb.length()-1);
-                }
-                else if (ch==']' && sb.charAt(sb.length()-1)=='[') {
-                    sb.deleteCharAt(sb.length()-1);
-                }
-                else if (ch=='}' && sb.charAt(sb.length()-1)=='{'){
-                    sb.deleteCharAt(sb.length()-1);
-                }
-                else{
-                    return false;
-                }
-            }
-            System.out.println();
-        }
-        return sb.length()==0;
-    }
-}
+// Last updated: 7/17/2026, 7:20:12 AM
+1class Solution {
+2    public boolean isValid(String s) {
+3        int n = s.length();
+4        Stack<Character> st = new Stack<>();
+5        for (int i=0 ; i<n ; i++){
+6            char ch = s.charAt(i);
+7            if (ch=='{' || ch=='[' || ch=='(' ){
+8                st.push(ch);
+9            }
+10            else{
+11                if (st.isEmpty()){
+12                    return false;
+13                }
+14                else{
+15                    char top = st.peek();
+16                    if ((ch=='}' && top=='{') || (ch==')' && top=='(') || (ch==']' && top=='[')){
+17                        st.pop();
+18                    }
+19                    else{
+20                        return false;
+21                    }
+22                }
+23            }
+24        
+25        }
+26        return st.isEmpty() ? true : false;
+27    }
+28
+29}
