c567b31468ba9e840b480959906511b4f76b8d1c
Sunil Singh <sunil.singh.030250@gmail.com>
2026-07-17 23:21:20 +0530

    File updated at 7/17/2026, 11:21:19 PM

--- 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.java
+++ 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.java
@@ -1,4 +1,4 @@
-// Last updated: 7/17/2026, 11:14:19 PM
+// Last updated: 7/17/2026, 11:21:19 PM
 1class Solution {
 2    public int longestIncreasingPath(int[][] matrix) {
 3	
@@ -10,46 +10,44 @@
 9        int[][] dp = new int[m][n];
 10        for (int i=0 ; i<m ; i++){
 11            for (int j=0 ; j<n ; j++){
-12                boolean[][] vis = new boolean[m][n];
-13                int currLen = helper(matrix,i,j,r,c,vis,dp);
-14                maxLen = Math.max(maxLen,currLen);
-15            }
-16        }
-17
-18        return maxLen;
-19        
-20    }
-21
-22    public int helper(int[][] matrix, int row, int col, int[] r, int[] c, boolean[][] vis, int[][] dp){
-23
-24        if (dp[row][col]!=0){
-25            return dp[row][col];
-26        }
-27
-28        vis[row][col] = true;
-29
-30        int currMaxLen = 1;
-31        
-32        for (int i=0 ; i<4 ; i++){
-33            int newR = row + r[i];
-34            int newC = col + c[i];
-35
-36            if (newR<0 || newC<0 || newR>=matrix.length || newC>=matrix[0].length ){
-37                continue;
-38            }
-39
-40            if (vis[newR][newC] || matrix[newR][newC]<=matrix[row][col]){
-41                continue;
-42            }
-43            
-44            int currRes = helper(matrix,newR,newC,r,c,vis,dp);
-45            currMaxLen = Math.max(currMaxLen,1+currRes);
-46        }
-47        
-48        vis[row][col] = false;
-49        return dp[row][col] =  currMaxLen;
-50        
-51    }
-52
-53
-54}
+12                int currLen = helper(matrix,i,j,r,c,dp);
+13                maxLen = Math.max(maxLen,currLen);
+14            }
+15        }
+16
+17        return maxLen;
+18        
+19    }
+20
+21    public int helper(int[][] matrix, int row, int col, int[] r, int[] c, int[][] dp){
+22
+23        if (dp[row][col]!=0){
+24            return dp[row][col];
+25        }
+26
+27        
+28
+29        int currMaxLen = 1;
+30        
+31        for (int i=0 ; i<4 ; i++){
+32            int newR = row + r[i];
+33            int newC = col + c[i];
+34
+35            if (newR<0 || newC<0 || newR>=matrix.length || newC>=matrix[0].length ){
+36                continue;
+37            }
+38
+39            if (matrix[newR][newC]<=matrix[row][col]){
+40                continue;
+41            }
+42            
+43            int currRes = helper(matrix,newR,newC,r,c,dp);
+44            currMaxLen = Math.max(currMaxLen,1+currRes);
+45        }
+46        
+47        return dp[row][col] =  currMaxLen;
+48        
+49    }
+50
+51
+52}
