29c8e9a783eb3ca5a2b82e01a2e2af3363d47154
Sunil Singh <sunil.singh.030250@gmail.com>
2026-07-20 15:39:12 +0530

    File updated at 7/20/2026, 3:39:10 PM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,28 +1,52 @@
-// Last updated: 9/30/2025, 7:33:36 AM
-class Solution {
-    public int numIslands(char[][] grid) {
-        int cnt = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-        for (int i=0 ; i<n ; i++){
-            for (int j=0 ; j<m ; j++){
-                if (grid[i][j]=='1'){
-                    cnt++;
-                    khatam(grid,i,j);
-                }
-            }
-        }
-        return cnt;
-    }
-    public static void khatam(char[][] grid , int cr , int cc){
-        if (cr<0 || cc<0 || cr>=grid.length || cc>=grid[0].length || grid[cr][cc]=='0'){
-            return;
-        }
-        grid[cr][cc] = '0';
-        int[] r = {1,-1,0,0};
-        int[] c = {0,0,1,-1};
-        for (int i=0 ; i<4 ; i++){
-            khatam(grid,cr+r[i],cc+c[i]);
-        }
-    }
-}
+// Last updated: 7/20/2026, 3:39:10 PM
+1class Solution {
+2    public int numIslands(char[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5        int cntIsland = 0;
+6        boolean[][] vis = new boolean[m][n];
+7            
+8        int[] dr = {-1,0,1,0};
+9        int[] dc = {0,1,0,-1};	
+10
+11        for (int i=0 ; i<m ; i++){
+12            for (int j=0 ; j<n ; j++){
+13                if (!vis[i][j] && grid[i][j]=='1'){
+14                    cntIsland++;
+15                    Queue<int[]> q = new LinkedList<>();
+16                    q.add(new int[]{i,j});
+17
+18                    while (!q.isEmpty()){
+19                        int[] cpos = q.poll();
+20                        int r = cpos[0];
+21                        int c = cpos[1];
+22                        if (vis[r][c]) continue;
+23                        
+24                        vis[r][c] = true;
+25                        
+26                        for (int idx=0 ; idx<4 ; idx++){
+27                            int newR = r + dr[idx];
+28                            int newC = c + dc[idx];
+29                            
+30                            if (newR<0 || newC<0 || newR>=m || newC>=n){
+31                                continue;
+32                            }
+33                                
+34                            if (!vis[newR][newC] && grid[newR][newC]=='1'){
+35                                q.add(new int[]{newR,newC});
+36                            }
+37                        }
+38                        
+39                    }
+40
+41
+42
+43
+44
+45                }
+46            }
+47        }
+48        return cntIsland;
+49    }
+50
+51}
