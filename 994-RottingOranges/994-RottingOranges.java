357e693fbbe6327d1567374a5bc293c9aefff48a
Sunil Singh <sunil.singh.030250@gmail.com>
2026-07-22 06:26:17 +0530

    File updated at 7/22/2026, 6:26:15 AM

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,54 +1,57 @@
-// Last updated: 10/17/2025, 12:41:42 PM
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        int fresh = 0;
-        
-        // Step 1: Add all rotten oranges initially to queue
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new Pair(i, j, 0));
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
-        }
-
-        // Step 2: BFS from all rotten oranges simultaneously
-        int time = 0;
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, -1, 0, 1};
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int r = p.r;
-            int c = p.c;
-            int dis = p.dis;
-            time = Math.max(time, dis);
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if (nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 1) {
-                    grid[nr][nc] = 2; // rot the fresh orange
-                    fresh--;
-                    q.offer(new Pair(nr, nc, dis + 1));
-                }
-            }
-        }
-
-        // Step 3: If any fresh orange remains, return -1
-        return fresh == 0 ? time : -1;
-    }
-}
-
-class Pair {
-    int r, c, dis;
-    Pair(int r, int c, int dis) {
-        this.r = r;
-        this.c = c;
-        this.dis = dis;
-    }
-}
+// Last updated: 7/22/2026, 6:26:15 AM
+1class Solution {
+2    public int orangesRotting(int[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5        int tcnt = 0;
+6        Queue<int[]> q = new LinkedList<>();
+7        for (int i=0 ; i<m ; i++){
+8            for (int j=0 ; j<n ; j++){
+9                if (grid[i][j]==2){
+10                    q.add(new int[]{i,j,0});
+11                    tcnt++;
+12                }
+13                else if (grid[i][j]==1){
+14                    tcnt++;
+15                }
+16            }
+17        }
+18
+19        boolean[][] vis = new boolean[m][n];
+20
+21
+22        int[] dr = {-1,0,1,0};
+23        int[] dc = {0,1,0,-1};
+24
+25        int minTime = 0;
+26
+27        while (!q.isEmpty()){
+28            int[] cp = q.poll();
+29            int r = cp[0];
+30            int c = cp[1];
+31            int t = cp[2];
+32
+33            if (vis[r][c]) continue;
+34            
+35            minTime = Math.max(minTime,t);
+36            
+37
+38            tcnt--;
+39            vis[r][c] = true;
+40            
+41            for (int i=0 ; i<4 ; i++){
+42                int newR = r + dr[i];
+43                int newC = c + dc[i];
+44
+45                if (newR<0 || newC<0 || newR>=m || newC>=n) continue;
+46
+47                if (vis[newR][newC] || grid[newR][newC]==0) continue;
+48
+49                q.add(new int[]{newR,newC,t+1});
+50            }
+51
+52        }
+53        
+54        return tcnt==0 ? minTime : -1;
+55    }
+56}
